8036f26e67390b6e09c665845854ae6a0d15b723
Erich Barnstedt <erichb@microsoft.com>
2017-05-23 11:03:51 +0200

    Upgrade to .NetCore 1.1 and updated to latest NuGet packages.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM microsoft/dotnet:1.0-sdk-projectjson
+FROM microsoft/dotnet:1.1-sdk-projectjson
 
 COPY / /build
 
@@ -8,6 +8,6 @@ RUN dotnet restore
 WORKDIR /build/src/GatewayApp.NetCore
 RUN dotnet publish
 
-WORKDIR /build/src/GatewayApp.NetCore/bin/Debug/netcoreapp1.0/publish
-ENV LD_LIBRARY_PATH=/build/src/GatewayApp.NetCore/bin/Debug/netcoreapp1.0/publish
+WORKDIR /build/src/GatewayApp.NetCore/bin/Debug/netcoreapp1.1/publish
+ENV LD_LIBRARY_PATH=/build/src/GatewayApp.NetCore/bin/Debug/netcoreapp1.1/publish
 ENTRYPOINT ["dotnet", "GatewayApp.NetCore.dll"]
--- src/GatewayApp.NetCore/Program.cs
+++ src/GatewayApp.NetCore/Program.cs
@@ -40,25 +40,9 @@ namespace GatewayApp.NetCore
             else
             {
                 Console.WriteLine("Target system is 32-bit.");
-                throw new Exception("32-bit systems are currently not supported due to https://github.com/Azure/azure-iot-gateway-sdk/releases#known-issues");
+                throw new Exception("32-bit systems are currently not supported.");
             }
             
-            // make sure our gateway runtime DLLs are in the current directory
-            string runtimesFolder = GetPathToRuntimesFolder(Directory.GetCurrentDirectory());
-            if (string.IsNullOrEmpty(runtimesFolder))
-            {
-                throw new Exception("Runtimes folder not found. Please make sure you have published the gateway app!");
-            }
-
-            // we always copy them across, overwriting existing ones, to make sure we have the right ones
-            string pathToNativeGatewayModules = runtimesFolder + Path.DirectorySeparatorChar + GetRuntimeID() + Path.DirectorySeparatorChar + "native";
-            List<string> filePaths = new List<string>(Directory.EnumerateFiles(pathToNativeGatewayModules));
-            foreach (string sourcefilePath in filePaths)
-            {
-                string destinationFilePath = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + Path.GetFileName(sourcefilePath);
-                File.Copy(sourcefilePath, destinationFilePath, true);
-            }
-
             // check if we got command line arguments to patch our gateway config file and register ourselves with IoT Hub
             if ((args.Length > 0) && !string.IsNullOrEmpty(args[0]))
             {
@@ -131,61 +115,10 @@ namespace GatewayApp.NetCore
                 Console.WriteLine(".NET Core Gateway failed to initialize.");
             }
         }
-
-        private static string GetPathToRuntimesFolder(string currentPath)
-        {
-            List<string> directories = new List<string>(Directory.EnumerateDirectories(currentPath));
-            foreach(string directoryPath in directories)
-            {
-                if (directoryPath.EndsWith("runtimes", StringComparison.OrdinalIgnoreCase))
-                {
-                    return directoryPath;
-                }
-
-                string result = GetPathToRuntimesFolder(directoryPath);
-                if (result != null)
-                {
-                    // found it
-                    return result;
-                }
-            }
-
-            return null;
-        }
-
+        
         private static bool IsX64Process()
         {
             return (IntPtr.Size == 8);
-        }
-
-        private static string GetRuntimeID()
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                string _OSDescription = RuntimeInformation.OSDescription.ToLowerInvariant();
-                if (_OSDescription.Contains("moby") || _OSDescription.Contains("debian"))
-                {
-                    // Moby (used by Docker) or Debian
-                    return "debian.8-x64";
-                }
-                else
-                {
-                    // fall back to Ubuntu, the only other Linux distro we support
-                    return "ubuntu.16.04-x64";
-                }
-            }
-            else
-            {
-                // fall back to Windows, the only other OS we support
-                if (IsX64Process())
-                {
-                    return "win-x64";
-                }
-                else
-                {
-                    return "win-x86";
-                }
-            }
         }
     }
 }
