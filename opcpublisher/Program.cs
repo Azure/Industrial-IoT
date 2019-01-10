edea6f5c96065270f519b9e1a8164ea660667a94
Hans Gschossmann <johanng@microsoft.com>
2019-01-10 12:16:06 +0100

    update version info to 2.3.0

--- opcpublisher/Program.cs
+++ opcpublisher/Program.cs
@@ -556,9 +556,9 @@ namespace OpcPublisher
                     return;
                 }
 
-                // start operation
+                // show version
                 Logger.Information($"OPC Publisher V{FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion} starting up...");
-                Logger.Debug($"Informational version: V{(Attribute.GetCustomAttribute(Assembly.GetEntryAssembly(), typeof(AssemblyInformationalVersionAttribute)) as AssemblyInformationalVersionAttribute).InformationalVersion} starting up...");
+                Logger.Debug($"Informational version: V{(Attribute.GetCustomAttribute(Assembly.GetEntryAssembly(), typeof(AssemblyInformationalVersionAttribute)) as AssemblyInformationalVersionAttribute).InformationalVersion}");
 
                 // allow canceling the application
                 var quitEvent = new ManualResetEvent(false);
@@ -784,6 +784,9 @@ namespace OpcPublisher
         {
 
             // show usage
+            Logger.Information("");
+            Logger.Information($"OPC Publisher V{FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion}");
+            Logger.Information($"Informational version: V{(Attribute.GetCustomAttribute(Assembly.GetEntryAssembly(), typeof(AssemblyInformationalVersionAttribute)) as AssemblyInformationalVersionAttribute).InformationalVersion}");
             Logger.Information("");
             Logger.Information("Usage: {0}.exe <applicationname> [<iothubconnectionstring>] [<options>]", Assembly.GetEntryAssembly().GetName().Name);
             Logger.Information("");
