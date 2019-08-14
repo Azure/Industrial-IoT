780eb5d71f949980b6930b0321724fa27d456735
Marc Schier <mschier@microsoft.com>
2019-08-14 17:40:56 +0200

    Fixdocker

--- tools/acrbuild.ps1
+++ tools/acrbuild.ps1
@@ -242,16 +242,7 @@ RUN apt-get update && apt-get install -y --no-install-recommends unzip curl proc
     && rm -rf /var/lib/apt/lists/* \
     && curl -sSL https://aka.ms/getvsdbgsh | bash /dev/stdin -v latest -l /vsdbg
 ENV PATH="${PATH}:/root/vsdbg/vsdbg"
-RUN apt-get update && apt-get install -y --no-install-recommends openssh-server \
-    && mkdir /var/run/sshd \
-    && echo 'root:Passw0rd' | chpasswd \
-    && sed -i 's/PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config \
-    && sed 's@session\s*required\s*pam_loginuid.so@session optional \
-        pam_loginuid.so@g' -i /etc/pam.d/sshd \
-    && echo "export VISIBLE=now" >> /etc/profile
-ENV NOTVISIBLE "in users profile"
 "@
-
     # Get project's assembly name to create entry point entry in dockerfile
     $assemblyName = $null
     ([xml] (Get-Content -Path $projFile.FullName)).Project.PropertyGroup `
