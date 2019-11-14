8e13f9d1622dccc392a5ae6235ac357d92fbc0c0
Karapet Kostandyan <kakostan@microsoft.com>
2019-11-14 12:19:55 +0100

    Feature/iai (#141)
    Added Microsoft.Azure.IIoT.Deployment.
    
    
    Added deployment of client and service applications.
    Added AKS cluster.
    Added integration of network components with AKS.
    Added logging using Serilog.
    Added environment variables of .env file.
    Added k8s deployment templates for micro-services.
    Fixed key retrieval of CosmosDB account.
    Moved configuration to one ConfigMap.
    Added resource limits.
    Added RedirectUrls for client application.
    Separated deployments and services yaml files.
    Added programmatic deployment of OMP services.
    Minor changes to OMP env vars generation.
    Added cert upload to AKS.
    Added client classes for KeyVault operations.
    Added IaiAksManagementClient and IaiEnvironment.
    Added IaiNetworkManagementClient.
    Renamed Omp* classes to Iai*.
    Added IaiWebSiteManagementClient.
    Added IaiApplicationInsightsManagementClient class.
    Added IaiOperationalInsightsManagementClient class.
    Added a few more Azure resource management classes.
    Consolidated app registration in IaiGraphServiceClient class.
    Added Azure client and configuration provider classes.
    Added DelegatingTokenProvider for TokenCredentials.
    Renamed project to Microsoft.Azure.IIoT.Deployment.
    Added Copyright header.
    Added DeploymentExecutor class.
    Added exception handling to Main().
    Added namespace separation.
    Got rid of Iai* prefix in class names.
    Removed mentions of omp from IIoTK8SClient.
    Renamed omp in k8s resources to industrial-iot.
    Enabled redirection of HTTP traffic to HTTPS for App Service.
    Upgraded Azure and Serilog nuget packages.
    Using portable BouncyCastle. Bumped version of Azure Authorization NuGet.
    Moved Microsoft.Azure.IIoT.Deployment project to deploy/src folder.
    Added resource cleanup flow on error.
    Changed order of resource cleanup.
    Added Build.props.
    Bumped Kubernetes version to 1.13.11.
    Migrated to .Net Core 3.0
    Added device code flow authentication for Unix systems.
    Reordered resource creation tasks to accelerate deployment.
    Using .Net Core 3.0 in Azure DevOps Pipelines.
    Improved comments and added new PasswordCredential creation flow.
    Added comments about installation of .Net Core 3.0 in DevOps steps.
    Signing IAI executable.
    Changed K8S IIoT env file to be Secret instead of ConfigMap.
    Added retry to RoleAssignment creation.
    Fixing Publish step of iai_publish.yml: so that it does not attempt to publish all projects.
    Added registration of resource providers.
    Fixing match pattern to only sign published exes.
    Commenting out everything.
    Changing order of DevOps jobs to execute iai_publish first.
    Forcing execution of iai_publish in .vsts-ci.yml
    Added ouputDir to publish task.
    Trying to set output directory via arguments.
    Disabling publish trimmer.
    Fixing executable signing and zip archive naming.
    Fixing versioning in zip file name.
    Building Microsoft.Azure.IIoT.Deployment on all platforms.
    Building Microsoft.Azure.IIoT.Deployment on all platforms.
    Running virus scan only on Windows.
    Fixing pattern to match linux and macOs executables.
    Only signing Windows executables.
    Added .Net Core 3.0 installation to iiot_deploy.
    Added md5 checksum generation on linux and macOS.
    Using separate job template for windows.
    Added linux and macOS executable publishing jobs.
    Minor changes to job names.
    Reverting back commented out jobs.
    Added documentation of Microsoft.Azure.IIoT.Deployment.
    Added sample helm install command for NGINX Ingress.
    Added note about required change in ingress YAML.
    Added note about restarting App Service.
    Fixed service names in Ingress.
    Removed specific hosts from tls spec of Ingress.
    Removed note about modifications to Ingress.
    Removed host URL part. That is no longer relevant since we set up the default SSL certificate for Ingress controller.
    Upgraded System.Private.Uri NuGet to version 4.3.2
    Fixing review comments regarding docs.
    Removed IndiaCentral region from list of functional regions: App Service Plan scaling is not available in the region.

---
+++ deploy/src/Microsoft.Azure.IIoT.Deployment/Infrastructure/AzureResourceManager.cs
@@ -0,0 +1,161 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.IIoT.Deployment.Infrastructure {
+
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.Azure.Management.Fluent;
+    using Microsoft.Azure.Management.ResourceManager.Fluent;
+    using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+    using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+    using Microsoft.Azure.Management.ResourceManager.Fluent.Models;
+    using Serilog;
+
+    class AzureResourceManager {
+
+        public static readonly Region[] FunctionalRegions = new Region[] {
+            Region.USEast2,
+            Region.USWest2,
+            Region.EuropeNorth,
+            Region.EuropeWest,
+            Region.CanadaCentral,
+            //Region.IndiaCentral, // App Service Plan scaling is not available in the region.
+            Region.AsiaSouthEast
+        };
+
+        private readonly Azure.IAuthenticated _authenticated;
+        private IAzure _azure;
+
+        public AzureResourceManager(
+            AzureCredentials azureCredentials
+        ) {
+            _authenticated = Azure
+                .Configure()
+                .Authenticate(azureCredentials);
+        }
+
+        public void Init(ISubscription subscription) {
+            _azure = _authenticated
+                .WithSubscription(subscription.SubscriptionId);
+        }
+
+        public IEnumerable<ITenant> GetTenants() {
+            var tenantsList = _authenticated
+                .Tenants
+                .List();
+
+            return tenantsList;
+        }
+
+        public IEnumerable<ISubscription> GetSubscriptions() {
+            var subscriptionsList = _authenticated
+                .Subscriptions
+                .List();
+
+            return subscriptionsList;
+        }
+
+        public IEnumerable<IResourceGroup> GetResourceGroups() {
+            var resourceGroups = _azure
+                .ResourceGroups
+                .List();
+
+            return resourceGroups;
+        }
+
+        public async Task<bool> CheckIfResourceGroupExistsAsync(
+            string resourceGroupName,
+            CancellationToken cancellationToken = default
+        ) {
+            var resourceGroupAlreadyExists = await _azure
+                .ResourceGroups
+                .ContainAsync(resourceGroupName, cancellationToken);
+
+            return resourceGroupAlreadyExists;
+        }
+
+        public async Task<IResourceGroup> CreateResourceGroupAsync(
+            Region region,
+            string resourceGroupName,
+            IDictionary<string, string> tags = null,
+            CancellationToken cancellationToken = default
+        ) {
+            try {
+                if (null == tags) {
+                    tags = new Dictionary<string, string> { };
+                }
+
+                Log.Information($"Creating Resource Group: {resourceGroupName} ...");
+
+                var resourceGroup = await _azure
+                    .ResourceGroups
+                    .Define(resourceGroupName)
+                    .WithRegion(region)
+                    .WithTags(tags)
+                    .CreateAsync(cancellationToken);
+
+                Log.Information($"Created Resource Group: {resourceGroupName}");
+
+                return resourceGroup;
+            }
+            catch (Exception ex) {
+                Log.Error(ex, $"Failed to create Resource Group: {resourceGroupName}");
+                throw;
+            }
+        }
+
+        public async Task BeginDeleteResourceGroupAsync(
+            IResourceGroup resourceGroup,
+            CancellationToken cancellationToken = default
+        ) {
+            try {
+                await _azure
+                    .ResourceGroups
+                    .BeginDeleteByNameAsync(
+                        resourceGroup.Name,
+                        cancellationToken
+                    );
+
+                Log.Information($"Initiated deletion of Resource Group: {resourceGroup.Name}");
+            }
+            catch (Exception ex) {
+                Log.Error(ex, $"Failed to initiate deletion of Resource Group: {resourceGroup.Name}");
+                throw;
+            }
+
+        }
+
+        public static async Task<IEnumerable<SubscriptionInner>> GetSubscriptionsUsingRestClientAsync(
+            AzureEnvironment azureEnvironment,
+            AzureCredentials azureCredentials,
+            CancellationToken cancellationToken = default
+        ) {
+            var restClient = RestClient
+                .Configure()
+                .WithEnvironment(azureEnvironment)
+                .WithCredentials(azureCredentials)
+                //.WithLogLevel(HttpLoggingDelegatingHandler.Level.BodyAndHeaders)
+                .Build();
+
+            var subscriptions = new List<SubscriptionInner>();
+
+            using (var subscriptionClient = new SubscriptionClient(restClient)) {
+                var subscriptionsList = await subscriptionClient
+                    .Subscriptions
+                    .ListAsync(cancellationToken);
+
+                foreach (var subscription in subscriptionsList) {
+                    subscriptions.Add(subscription);
+                }
+            };
+
+            return subscriptions;
+        }
+    }
+}
