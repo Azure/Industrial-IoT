26f627976e2dee3286f2e0629318b5c07e1faf26
Markus Stuhler <mstuhler@microsoft.com>
2020-02-06 14:32:17 +0100

    Updated security implementation

---
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Protocol/src/Exceptions/EndpointNotAvailableException.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.IIoT.OpcUa.Core.Models;
+using Opc.Ua;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.IIoT.OpcUa.Protocol.Exceptions {
+    /// <summary>
+    /// Exception when an endpoint with requested security settings is not available.
+    /// </summary>
+    public class EndpointNotAvailableException : Exception {
+        /// <summary>
+        /// Creates a new instance of EndpointNotAvailableException.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="securityMode"></param>
+        /// <param name="securityPolicyUrl"></param>
+        public EndpointNotAvailableException(string url, SecurityMode? securityMode, string securityPolicyUrl) : base($"There is not endpoint with requested security settings (SecurityMode: {securityMode}, SecurityPolicyUrl: {securityPolicyUrl}) available at url '{url}'.") { }
+    }
+}
