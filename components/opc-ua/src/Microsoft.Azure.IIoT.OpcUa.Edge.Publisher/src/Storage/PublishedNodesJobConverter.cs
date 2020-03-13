0599373b9a7e083631d587e89a83a37dba6eb91d
Cristian Pogacean <crpogace@microsoft.com>
2020-03-13 17:54:41 +0100

    consistent behavior of telemetry processors (#304)
    * consistent behavior of telemetry processors
    * standalone publisher IDs changed to identity

--- components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Edge.Publisher/src/Storage/PublishedNodesJobConverter.cs
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Edge.Publisher/src/Storage/PublishedNodesJobConverter.cs
@@ -30,11 +30,13 @@ namespace Microsoft.Azure.IIoT.OpcUa.Publisher.Models {
         /// <param name="logger"></param>
         /// <param name="config"></param>
         /// <param name="cryptoProvider"></param>
-        public PublishedNodesJobConverter(ILogger logger,
+        /// <param name="identity"></param>
+        public PublishedNodesJobConverter(ILogger logger, IIdentity identity,
             IEngineConfiguration config = null, ISecureElement cryptoProvider = null) {
             _config = config;
             _cryptoProvider = cryptoProvider;
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _identity = identity ?? throw new ArgumentNullException(nameof(identity));
         }
 
         /// <summary>
@@ -126,38 +128,46 @@ namespace Microsoft.Azure.IIoT.OpcUa.Publisher.Models {
                             DiagnosticsInterval = _config.DiagnosticsInterval
                         },
                         WriterGroup = new WriterGroupModel {
-                            WriterGroupId = null,
+                            WriterGroupId = _identity.DeviceId + "_"+ _identity.ModuleId,
                             DataSetWriters = new List<DataSetWriterModel> {
                                 new DataSetWriterModel {
-                                    DataSetWriterId = Guid.NewGuid().ToString(),
+                                    DataSetWriterId = _identity.DeviceId + "_"+ _identity.ModuleId,
                                     DataSet = new PublishedDataSetModel {
                                         DataSetSource = dataSetSource.Clone()
                                     },
-                                    DataSetFieldContentMask = DataSetFieldContentMask.SourceTimestamp | 
-                                        DataSetFieldContentMask.ServerTimestamp | 
+                                    DataSetFieldContentMask = 
                                         DataSetFieldContentMask.StatusCode |
-                                        DataSetFieldContentMask.NodeId | 
+                                        DataSetFieldContentMask.SourceTimestamp |
+                                        DataSetFieldContentMask.ServerTimestamp |
+                                        DataSetFieldContentMask.NodeId |
+                                        DataSetFieldContentMask.DisplayName |
                                         DataSetFieldContentMask.ApplicationUri |
-                                        DataSetFieldContentMask.DisplayName,
+                                        DataSetFieldContentMask.EndpointUrl |
+                                        DataSetFieldContentMask.SubscriptionId |
+                                        DataSetFieldContentMask.ExtraFields,
                                     MessageSettings = new DataSetWriterMessageSettingsModel() {
-                                        DataSetMessageContentMask = DataSetContentMask.Timestamp | 
-                                        DataSetContentMask.MetaDataVersion | 
-                                        DataSetContentMask.Status | 
-                                        DataSetContentMask.DataSetWriterId | 
-                                        DataSetContentMask.MajorVersion | 
-                                        DataSetContentMask.MinorVersion | 
-                                        DataSetContentMask.SequenceNumber
+                                        DataSetMessageContentMask =
+                                            DataSetContentMask.Timestamp |
+                                            DataSetContentMask.MetaDataVersion |
+                                            DataSetContentMask.Status |
+                                            DataSetContentMask.DataSetWriterId |
+                                            DataSetContentMask.MajorVersion |
+                                            DataSetContentMask.MinorVersion |
+                                            DataSetContentMask.SequenceNumber
                                     }
                                 }
                             },
                             MessageSettings = new WriterGroupMessageSettingsModel() {
-                                NetworkMessageContentMask = NetworkMessageContentMask.PublisherId | 
-                                NetworkMessageContentMask.WriterGroupId | 
-                                NetworkMessageContentMask.SequenceNumber | 
-                                NetworkMessageContentMask.PayloadHeader | 
-                                NetworkMessageContentMask.NetworkMessageHeader |
-                                NetworkMessageContentMask.Timestamp | 
-                                NetworkMessageContentMask.DataSetMessageHeader
+                                NetworkMessageContentMask =
+                                    NetworkMessageContentMask.PublisherId |
+                                    NetworkMessageContentMask.WriterGroupId |
+                                    NetworkMessageContentMask.NetworkMessageNumber |
+                                    NetworkMessageContentMask.SequenceNumber |
+                                    NetworkMessageContentMask.PayloadHeader |
+                                    NetworkMessageContentMask.Timestamp |
+                                    NetworkMessageContentMask.DataSetClassId |
+                                    NetworkMessageContentMask.NetworkMessageHeader |
+                                    NetworkMessageContentMask.DataSetMessageHeader
                             }
                         }
                     }));
@@ -343,5 +353,6 @@ namespace Microsoft.Azure.IIoT.OpcUa.Publisher.Models {
         private readonly IEngineConfiguration _config;
         private readonly ISecureElement _cryptoProvider;
         private readonly ILogger _logger;
+        private readonly IIdentity _identity;
     }
 }
--- components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa/src/Subscriber/Models/MonitoredItemSampleModel.cs
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa/src/Subscriber/Models/MonitoredItemSampleModel.cs
@@ -16,6 +16,7 @@ namespace Microsoft.Azure.IIoT.OpcUa.Subscriber.Models {
         /// Node id
         /// </summary>
         public string NodeId { get; set; }
+
         /// <summary>
         /// Display name
         /// </summary>
