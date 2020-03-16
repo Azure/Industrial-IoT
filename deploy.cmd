6f28afa01fde57b15338b20624d7953e3693bf1a
Marc Schier <marcschier@hotmail.com>
2020-03-16 12:34:01 +0100

    Documentation and deployment script fixes

--- deploy.cmd
+++ deploy.cmd
@@ -7,7 +7,26 @@
 set current-path=%~dp0
 rem // remove trailing slash
 set current-path=%current-path:~0,-1%
-pushd %current-path%\deploy\scripts
 shift
+
+:check-az
+set test=
+for /f %%i in ('powershell -Command "Get-Module -ListAvailable -Name Az.* | ForEach-Object Name"') do set test=%%i
+if not "%test%" == "" goto :check-ad
+echo Installing Az...
+powershell -Command "Install-Module -Name Az -AllowClobber -Scope CurrentUser"
+goto :check-ad
+:check-ad
+echo Az installed.
+set test=
+for /f %%i in ('powershell -Command "Get-Module -ListAvailable -Name AzureAD | ForEach-Object Name"') do set test=%%i
+if not "%test%" == "" goto :main
+echo Installing AzureAD
+powershell -Command "Install-Module -Name AzureAD -AllowClobber -Scope CurrentUser"
+goto :main
+:main
+echo AzureAD installed.
+set test=
+pushd %current-path%\deploy\scripts
 powershell -ExecutionPolicy Unrestricted ./deploy.ps1 %*
 popd
