bababe68d29b5bad0807779c5050629335769c6e
Cristian Pogacean <crpogace@microsoft.com>
2020-03-17 16:28:46 +0100

    added IPV6 as fallback to Discovery urls

--- components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Edge.Discovery/src/Services/DiscoveryServices.cs
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Edge.Discovery/src/Services/DiscoveryServices.cs
@@ -438,11 +438,20 @@ namespace Microsoft.Azure.IIoT.OpcUa.Edge.Discovery.Services {
         private Task<(IPEndPoint, Uri)> GetHostEntryAsync(
             Uri discoveryUrl) {
             return Try.Async(async () => {
-
                 var entry = await Dns.GetHostEntryAsync(discoveryUrl.DnsSafeHost);
                 // only pick-up the IPV4 addresses
                 foreach (var address in entry.AddressList
                     .Where(a => a.AddressFamily == AddressFamily.InterNetwork)) {
+                    var reply = await new Ping().SendPingAsync(address);
+                    if (reply.Status == IPStatus.Success) {
+                        var ep = new IPEndPoint(address,
+                            discoveryUrl.IsDefaultPort ? 4840 : discoveryUrl.Port);
+                        return (ep, discoveryUrl);
+                    }
+                }
+                // if no IPV4 responsive, try IPV6 as fallback
+                foreach (var address in entry.AddressList
+                    .Where(a => a.AddressFamily != AddressFamily.InterNetwork)) {
                     var reply = await new Ping().SendPingAsync(address);
                     if (reply.Status == IPStatus.Success) {
                         var ep = new IPEndPoint(address,
