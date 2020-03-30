35f0b518890ea5b06923582049bb66c9ee81115b
Marc Schier <marcschier@hotmail.com>
2020-03-30 09:41:15 +0200

    Fix perf

--- samples/src/Microsoft.Azure.IIoT.App/src/Services/Registry.cs
+++ samples/src/Microsoft.Azure.IIoT.App/src/Services/Registry.cs
@@ -121,8 +121,8 @@ namespace Microsoft.Azure.IIoT.App.Services {
 
                 if (applications != null) {
                     foreach (var app in applications) {
-                        var application = app; //  await _registryService.GetApplicationAsync(app.ApplicationId);
-                        pageResult.Results.Add(application.Application);
+                        var application = app; // (await _registryService.GetApplicationAsync(app.ApplicationId)).Application;
+                        pageResult.Results.Add(application);
                     }
                 }
             }
