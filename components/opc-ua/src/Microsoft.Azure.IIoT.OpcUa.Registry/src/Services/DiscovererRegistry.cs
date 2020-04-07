e158a88842509e1425d8a50a79815e01bf82a0cf
Daniele Colonna <dacol@microsoft.com>
2020-04-07 09:36:25 +0200

    fix (#374)
    - invalid url when page is manually refreshed
    - discovery url parameter
    
    Co-authored-by: Marc Schier <marcschier@users.noreply.github.com>

--- components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Registry/src/Services/DiscovererRegistry.cs
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Registry/src/Services/DiscovererRegistry.cs
@@ -143,6 +143,11 @@ namespace Microsoft.Azure.IIoT.OpcUa.Registry.Services {
                                 request.DiscoveryConfig.ActivationFilter.TrustLists == null ?
                                     null : request.DiscoveryConfig.ActivationFilter;
                         }
+                        if (request.DiscoveryConfig.DiscoveryUrls != null) {
+                            patched.RequestedConfig.DiscoveryUrls =
+                                request.DiscoveryConfig.DiscoveryUrls.Count == 0 ?
+                                    null : request.DiscoveryConfig.DiscoveryUrls;
+                        }
                     }
                     // Patch
                     await _iothub.PatchAsync(registration.Patch(
