4ada594a01d2f3f33a6aa0ddcad8c1f6568162b6
Monika <movarshn@microsoft.com>
2020-05-04 17:42:13 +0200

    fix for edge metrics

--- common/src/Microsoft.Azure.IIoT.Hub.Module.Client/src/Extensions/MetricServerEx.cs
+++ common/src/Microsoft.Azure.IIoT.Hub.Module.Client/src/Extensions/MetricServerEx.cs
@@ -5,6 +5,7 @@
 
 namespace Microsoft.Azure.IIoT.Module.Framework.Client {
     using Prometheus;
+    using System.Net;
 
     /// <summary>
     /// Metric Server extensions
@@ -17,8 +18,17 @@ namespace Microsoft.Azure.IIoT.Module.Framework.Client {
         /// <returns></returns>
         public static void StartWhenEnabled(this IMetricServer server, IModuleConfig config, Serilog.ILogger logger) {
             if (config.EnableMetrics) {
-                server.Start();
-                logger.Information("Prometheus metric server started.");
+                try {
+                    server.Start();
+                    logger.Information("Prometheus metric server started.");
+                }
+                catch (HttpListenerException e){
+                    logger.Error(e, "Unable to start metric server. To enable edge metrics collection, please run the following commands in an elevated window:\n" +
+                        "\nnetsh http add urlacl url=http://+:9700/metrics user=Everyone" +
+                        "\nnetsh http add urlacl url=http://+:9701/metrics user=Everyone" +
+                        "\nnetsh http add urlacl url=http://+:9702/metrics user=Everyone\n");
+                }
+
             } 
             else {
                 logger.Information("Metrics Collection is disabled. Not starting prometheus metric server.");
