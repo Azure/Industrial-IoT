d13faea35bc900160e26795c12cdb5d3c0dd41f9
Cristian Pogacean <crpogace@microsoft.com>
2020-05-08 13:49:46 +0200

    Hotfix/certificates (#423)
    * unifying opc ua certificates handling
    * added PkiRoot config
    * fixed tests in build pipeline
    * consistently use the Dns.GetHostName()
    * pki certificate stores everywhere
    * cleanup IClientServicesConfig
    
    Co-authored-by: Marc Schier <marcschier@users.noreply.github.com>

--- modules/src/Microsoft.Azure.IIoT.Modules.OpcUa.Twin/src/Runtime/Config.cs
+++ modules/src/Microsoft.Azure.IIoT.Modules.OpcUa.Twin/src/Runtime/Config.cs
@@ -11,14 +11,11 @@ namespace Microsoft.Azure.IIoT.Modules.OpcUa.Twin.Runtime {
     using Microsoft.Azure.IIoT.Hub.Module.Client.Runtime;
     using Microsoft.Azure.IIoT.Diagnostics;
     using Microsoft.Extensions.Configuration;
-    using System.Runtime.InteropServices;
-    using System;
 
     /// <summary>
     /// Wraps a configuration root
     /// </summary>
-    public class Config : DiagnosticsConfig, IModuleConfig, IClientServicesConfig2,
-        ISecurityConfig, ITransportQuotaConfig, IClientServicesConfig {
+    public class Config : DiagnosticsConfig, IModuleConfig, IClientServicesConfig{
 
         /// <inheritdoc/>
         public string EdgeHubConnectionString => _module.EdgeHubConnectionString;
@@ -42,6 +39,8 @@ namespace Microsoft.Azure.IIoT.Modules.OpcUa.Twin.Runtime {
         public uint MaxKeepAliveCount => _opc.MaxKeepAliveCount;
         /// <inheritdoc/>
         public int MinSubscriptionLifetime => _opc.MinSubscriptionLifetime;
+        /// <inheritdoc/>
+        public string PkiRootPath => _opc.PkiRootPath;
         /// <inheritdoc/>
         public CertificateInfo ApplicationCertificate => _opc.ApplicationCertificate;
         /// <inheritdoc/>
@@ -74,61 +73,16 @@ namespace Microsoft.Azure.IIoT.Modules.OpcUa.Twin.Runtime {
         public int SecurityTokenLifetime => _opc.SecurityTokenLifetime;
 
         /// <summary>
-        /// ClientServicesConfig
-        /// </summary>
-        private const string kAppCertStoreType = "AppCertStoreType";
-        private const string kPkiRootPath = "PkiRootPath";
-        private const string kOwnCertPath = "OwnCertPath";
-        private const string kTrustedCertPath = "TrustedCertPath";
-        private const string kIssuerCertPath = "IssuerCertPath";
-        private const string kRejectedCertPath = "RejectedCertPath";
-        private const string kAutoAccept = "AutoAccept";
-        private const string kOwnCertX509StorePathDefault = "OwnCertX509StorePathDefault";
-        private const string kSessionTimeout = "SessionTimeout";
-        private const string kOperationTimeout = "OperationTimeout";
-
-        /// <inheritdoc/>
-        public string AppCertStoreType => GetStringOrDefault(kAppCertStoreType,
-            () => RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "X509Store" : "Directory");
-        /// <inheritdoc/>
-        public string PkiRootPath =>
-            GetStringOrDefault(kPkiRootPath, () => "pki");
-        /// <inheritdoc/>
-        public string OwnCertPath =>
-            GetStringOrDefault(kOwnCertPath, () => PkiRootPath + "/own");
-        /// <inheritdoc/>
-        public string TrustedCertPath =>
-            GetStringOrDefault(kTrustedCertPath, () => PkiRootPath + "/trusted");
-        /// <inheritdoc/>
-        public string IssuerCertPath =>
-            GetStringOrDefault(kIssuerCertPath, () => PkiRootPath + "/issuer");
-        /// <inheritdoc/>
-        public string RejectedCertPath =>
-            GetStringOrDefault(kRejectedCertPath, () => PkiRootPath + "/rejected");
-        /// <inheritdoc/>
-        public string OwnCertX509StorePathDefault =>
-            GetStringOrDefault(kOwnCertX509StorePathDefault, () => "CurrentUser\\UA_MachineDefault");
-        /// <inheritdoc/>
-        bool IClientServicesConfig.AutoAcceptUntrustedCertificates =>
-            GetBoolOrDefault(kAutoAccept, () => false);
-        /// <inheritdoc/>
-        TimeSpan? IClientServicesConfig.DefaultSessionTimeout =>
-            GetDurationOrNull(kSessionTimeout);
-        /// <inheritdoc/>
-        TimeSpan? IClientServicesConfig.OperationTimeout =>
-            GetDurationOrNull(kOperationTimeout);
-
-        /// <summary>
         /// Configuration constructor
         /// </summary>
         /// <param name="configuration"></param>
         public Config(IConfiguration configuration) :
             base(configuration) {
             _module = new ModuleConfig(configuration);
-            _opc = new ClientServicesConfig2(configuration);
+            _opc = new ClientServicesConfig(configuration);
         }
 
-        private readonly ClientServicesConfig2 _opc;
+        private readonly ClientServicesConfig _opc;
         private readonly ModuleConfig _module;
     }
 }
