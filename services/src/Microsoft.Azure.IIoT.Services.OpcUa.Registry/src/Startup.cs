f5f2cca75f4ed92bf1b3c4f5907cee53bef101f5
Monika Varshney <9294861+monikavar@users.noreply.github.com>
2020-05-26 17:33:39 +0200

    metrics for response time of cloud micro-services API calls (#494)
    * adding metrics to microservices
    
    * update publisher metrics, add start time

--- services/src/Microsoft.Azure.IIoT.Services.OpcUa.Registry/src/Startup.cs
+++ services/src/Microsoft.Azure.IIoT.Services.OpcUa.Registry/src/Startup.cs
@@ -147,6 +147,7 @@ namespace Microsoft.Azure.IIoT.Services.OpcUa.Registry {
             app.UseCorrelation();
             app.UseSwagger();
             app.UseMetricServer();
+            app.UseHttpMetrics();
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
                 endpoints.MapHealthChecks("/healthz");
--- services/src/Microsoft.Azure.IIoT.Services.OpcUa.Vault/src/Startup.cs
+++ services/src/Microsoft.Azure.IIoT.Services.OpcUa.Vault/src/Startup.cs
@@ -147,6 +147,7 @@ namespace Microsoft.Azure.IIoT.Services.OpcUa.Vault {
             app.UseCorrelation();
             app.UseSwagger();
             app.UseMetricServer();
+            app.UseHttpMetrics();
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
                 endpoints.MapHealthChecks("/healthz");
