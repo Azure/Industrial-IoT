89e2b0fa8d18a7eb0733081faa8639f93714f208
MaidaB <maidabakovic@gmail.com>
2020-06-03 11:51:53 +0200

    DiscovererInfo validation

--- samples/src/Microsoft.Azure.IIoT.App/src/Models/DiscovererInfoValidator.cs
+++ samples/src/Microsoft.Azure.IIoT.App/src/Models/DiscovererInfoValidator.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using FluentValidation;
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
 
 namespace Microsoft.Azure.IIoT.App.Models
 {
+    using System;
+    using FluentValidation;
+    using Microsoft.Azure.IIoT.Net.Models;
+
     public class DiscovererInfoValidator : AbstractValidator<DiscovererInfoRequested>
     {
         public DiscovererInfoValidator()
         {
+            RuleFor(p => p.RequestedAddressRangesToScan)
+                .Must(BeValidAddressRanges)
+                .WithMessage("Invalid input value for address ranges.");
+
+            RuleFor(p => p.RequestedPortRangesToScan)
+                .Must(BeValidPortRanges)
+                .WithMessage("Invalid input value for port ranges.");
+
+            RuleFor(p => p.RequestedMaxNetworkProbes)
+                .Must(BeAPositiveInteger)
+                .WithMessage("Max network probes must be a positive integer.");
+
+            RuleFor(p => p.RequestedMaxPortProbes)
+                .Must(BeAPositiveInteger)
+                .WithMessage("Max port probes must be a positive integer.");
+
             RuleFor(p => p.RequestedNetworkProbeTimeout)
                 .Must(BeAValidTimeFormat)
                 .WithMessage("Invalid input value for network probe timeout.");
@@ -18,39 +40,44 @@ namespace Microsoft.Azure.IIoT.App.Models
             RuleFor(p => p.RequestedIdleTimeBetweenScans)
                 .Must(BeAValidTimeFormat)
                 .WithMessage("Invalid input value for idle time between scans.");
-
-            RuleFor(p => p.RequestedMaxNetworkProbes)
-                .Must(BeAPositiveNumber)
-                .WithMessage("Requested max network probes must be a positive integer.");
-
-            RuleFor(p => p.RequestedMaxPortProbes)
-                .Must(BeAPositiveNumber)
-                .WithMessage("Requested max port probes must be a positive integer.");
-
-            RuleFor(p => p.RequestedAddressRangesToScan)
-                .Must(BeAPositiveNumber)
-                .WithMessage("Requested address ranges to scan must be a positive integer.");
-
-            RuleFor(p => p.RequestedPortRangesToScan)
-                .Must(BeAPositiveNumber)
-                .WithMessage("Requested port ranges to schan must be a positive integer.");
         }
 
-        private bool BeAValidTimeFormat(string value)
+        /// <summary>
+        /// Checks if the default value should be used. The default value 
+        /// should be used when the user did not input any value
+        /// </summary>
+        /// <param name="value">User input</param>
+        /// <returns>True if the input is empty, false otherwise</returns>
+        private bool ShouldUseDefaultValue(string value)
         {
-            if (string.IsNullOrEmpty(value))
+            // User did not input the value, so the default value will be used
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        private bool BeValidAddressRanges(string value)
+        {
+            if (ShouldUseDefaultValue(value))
             {
-                // User did not input the value, so the default value will be used
                 return true;
             }
-            return TimeSpan.TryParse(value, out TimeSpan res);
+
+            return AddressRange.TryParse(value, out _);
         }
 
-        private bool BeAPositiveNumber(string value)
+        private bool BeValidPortRanges(string value)
         {
-            if (string.IsNullOrEmpty(value))
+            if (ShouldUseDefaultValue(value))
             {
-                // User did not input the value, so the default value will be used
+                return true;
+            }
+
+            return PortRange.TryParse(value, out _);
+        }
+
+        private bool BeAPositiveInteger(string value)
+        {
+            if (ShouldUseDefaultValue(value))
+            {
                 return true;
             }
 
@@ -60,6 +87,16 @@ namespace Microsoft.Azure.IIoT.App.Models
             }
 
             return false;
+        }
+
+        private bool BeAValidTimeFormat(string value)
+        {
+            if (ShouldUseDefaultValue(value))
+            {
+                return true;
+            }
+
+            return TimeSpan.TryParse(value, out _);
         }
     }
 }
