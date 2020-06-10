3e238159b3bb9834d042dca55e0905989defe416
Luis Cantero <lcantero@microsoft.com>
2020-06-10 11:20:05 +0200

    Add razor code-behind pages and connection status icons (#544)
    * Add code-behind file to razor pages
    * Apply VS suggestions to Razor code-behind files
    * Add icon for connection status
    * Fix some inconsistencies with property/field conventions
    
    Co-authored-by: Daniele Colonna <dacol@microsoft.com>

---
+++ samples/src/Microsoft.Azure.IIoT.App/src/Pages/Gateways.razor.cs
@@ -0,0 +1,77 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.IIoT.App.Pages {
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.Azure.IIoT.App.Data;
+    using Microsoft.Azure.IIoT.OpcUa.Api.Registry.Models;
+    using Microsoft.Azure.IIoT.OpcUa.Api.Registry;
+
+    public partial class Gateways {
+        [Parameter]
+        public string Page { get; set; } = "1";
+
+        private PagedResult<GatewayApiModel> GatewayList { get; set; } = new PagedResult<GatewayApiModel>();
+        private PagedResult<GatewayApiModel> _pagedGatewayList = new PagedResult<GatewayApiModel>();
+        private IAsyncDisposable _gatewayEvent;
+        private string _tableView = "visible";
+        private string _tableEmpty = "displayNone";
+
+        /// <summary>
+        /// Notify page change
+        /// </summary>
+        /// <param name="page"></param>
+        public async Task PagerPageChangedAsync(int page) {
+            CommonHelper.Spinner = "loader-big";
+            StateHasChanged();
+            GatewayList = CommonHelper.UpdatePage(RegistryHelper.GetGatewayListAsync, page, GatewayList, ref _pagedGatewayList, CommonHelper.PageLength);
+            NavigationManager.NavigateTo(NavigationManager.BaseUri + "gateways/" + page);
+            for (int i = 0; i < _pagedGatewayList.Results.Count; i++) {
+                _pagedGatewayList.Results[i] = (await RegistryService.GetGatewayAsync(_pagedGatewayList.Results[i].Id)).Gateway;
+            }
+            CommonHelper.Spinner = string.Empty;
+            StateHasChanged();
+        }
+
+        /// <summary>
+        /// OnInitialized
+        /// </summary>
+        protected override void OnInitialized() {
+            CommonHelper.Spinner = "loader-big";
+        }
+
+        /// <summary>
+        /// OnAfterRenderAsync
+        /// </summary>
+        /// <param name="firstRender"></param>
+        protected override async Task OnAfterRenderAsync(bool firstRender) {
+            if (firstRender) {
+                GatewayList = await RegistryHelper.GetGatewayListAsync();
+                Page = "1";
+                _pagedGatewayList = GatewayList.GetPaged(int.Parse(Page), CommonHelper.PageLength, GatewayList.Error);
+                CommonHelper.Spinner = string.Empty;
+                CommonHelper.CheckErrorOrEmpty(_pagedGatewayList, ref _tableView, ref _tableEmpty);
+                StateHasChanged();
+                _gatewayEvent = await RegistryServiceEvents.SubscribeGatewayEventsAsync(
+                    ev => InvokeAsync(() => GatewayEvent(ev)));
+            }
+        }
+
+        private Task GatewayEvent(GatewayEventApiModel ev) {
+            GatewayList.Results.Update(ev);
+            _pagedGatewayList = GatewayList.GetPaged(int.Parse(Page), CommonHelper.PageLength, GatewayList.Error);
+            StateHasChanged();
+            return Task.CompletedTask;
+        }
+
+        public async void Dispose() {
+            if (_gatewayEvent != null) {
+                await _gatewayEvent.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ samples/src/Microsoft.Azure.IIoT.App/src/Shared/TSILink.razor.cs
@@ -0,0 +1,31 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.IIoT.App.Shared {
+    using System;
+
+    public partial class TSILink {
+        private string TsiLink { get; set; } = null;
+
+        /// <summary>
+        /// OnInitialized
+        /// </summary>
+        protected override void OnInitialized() {
+            CreateTSILink();
+        }
+
+        /// <summary>
+        /// Create TSI link
+        /// </summary>
+        private void CreateTSILink() {
+            if (!string.IsNullOrEmpty(Configuration.TsiDataAccessFQDN)) {
+                var index = Configuration.TsiDataAccessFQDN.IndexOf('.');
+                if (index > 0) {
+                    TsiLink = "https://insights.timeseries.azure.com/preview?environmentId=" + Configuration.TsiDataAccessFQDN.Substring(0, index) + "&tid=" + Configuration.TenantId;
+                }
+            }
+        }
+    }
+}
