33150f9253d2dc41bd10288878f706f743758569
Cristian Pogacean <crpogace@microsoft.com>
2020-06-16 11:47:12 +0200

    typo

--- components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Protocol/src/Stack/Encoders/JsonDecoderEx.cs
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Protocol/src/Stack/Encoders/JsonDecoderEx.cs
@@ -1,7 +1,7 @@
 // ------------------------------------------------------------
 //  Copyright (c) Microsoft Corporation.  All rights reserved.
 //  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
-// -------------------------------------------------5-----------
+// ------------------------------------------------------------
 
 namespace Opc.Ua.Encoders {
     using Opc.Ua.Extensions;
