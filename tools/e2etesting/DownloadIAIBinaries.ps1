3ec68b0171d31afda39224cacb35b1cb901e6425
Markus Stuhler <mstuhler@microsoft.com>
2020-07-15 13:46:35 +0200

    Updated YAML and PS1 for IAI

---
+++ tools/e2etesting/DownloadIAIBinaries.ps1
@@ -0,0 +1,40 @@
+param(
+    [string]
+    $IAIBlobUrl = "https://azureiiot.blob.core.windows.net/binaries/master/",
+
+    [string]
+    $IAIVersion,
+
+    [string]
+    $IAILocalFolder
+)
+
+# $IAIVersion = "2.7.160"
+if (!$IAILocalFolder) {
+    $IAILocalFolder = $PSScriptRoot
+    Write-Host "##vso[task.logissue type=warning]IAILocalFolder not set, using $($IAILocalFolder)."
+}
+
+if (!$IAIVersion) {
+    Write-Host "##vso[task.complete result=Failed]IAIVersion not set, exiting."
+}
+
+$iaiBinaryFilename = "Microsoft.Azure.IIoT.Deployment.exe"
+$iaiUrlPostfix = "/win-x64/" + $iaiBinaryFilename
+
+$iaiFullUrl = $IAIBlobUrl.TrimEnd('/') + "/" + $IAIVersion + "/" + $iaiUrlPostfix.TrimStart('/')
+$iaiLocalFilename = [System.IO.Path]::Combine($IAILocalFolder, $iaiBinaryFilename)
+
+Write-Host "##[group]Downloading IAI binaries..."
+Write-Host "IAI binary download"
+Write-Host "  Source: $($iaiFullUrl)"
+Write-Host "  Target: $($iaiLocalFilename)"
+
+Write-Host "##[command]Downloading binary..."
+(New-Object System.Net.WebClient).DownloadFile($iaiFullUrl, $iaiLocalFilename)
+
+Write-Host "Download complete"
+Write-Host "##[endgroup]"
+
+Write-Host "Settings Pipelines-Variable 'IAILocalFilename' to $($iaiLocalFilename)..."
+Write-Host "##vso[task.setvariable variable=IAILocalFilename;]$($iaiLocalFilename)"
