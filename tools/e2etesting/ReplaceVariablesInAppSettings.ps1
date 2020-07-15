8cda6e98f0d91ef0fb5852e24fd785976de0d508
Markus Stuhler <mstuhler@microsoft.com>
2020-07-15 14:45:22 +0200

    Updated appSettings.json processing

---
+++ tools/e2etesting/ReplaceVariablesInAppSettings.ps1
@@ -0,0 +1,57 @@
+Param(
+    [Guid]
+    $TenantId,
+    [Guid]
+    $SubscriptionId,
+    [Guid]
+    $ClientId,
+    [string]
+    $ClientSecret,
+    [string]
+    $ApplicationName,
+    [string]
+    $AppSettingsFilename
+)
+
+if (!$TenantId -or !$SubscriptionId) {
+    Write-Host "Getting Azure Context..."
+    $context = Get-AzureRmContext
+
+    if (!$SubscriptionId) {
+        Write-Host "Getting Subscription information..."
+        $subscription = Get-AzureRmSubscription -Name $context.SubscriptionName
+        $SubscriptionId = $subscription.Id
+        Write-Host "Using Subscription Id $($subscriptionId)."
+    }
+
+    if (!$TenantId) {
+        $TenantId = $context.TenantId
+        Write-Host "Using TenantId $($TenantId)."
+    }
+}
+
+if (!$ClientId) {
+    Write-Host "##vso[task.complete result=Failed]ClientId not set, exiting."
+}
+
+if (!$ClientSecret) {
+    Write-Host "##vso[task.complete result=Failed]ClientSecret not set, exiting."
+}
+
+if (!$ApplicationName) {
+    Write-Host "##vso[task.complete result=Failed]ApplicationName not set, exiting."
+}
+
+if (!$AppSettingsFilename) {
+    Write-Host "##vso[task.complete result=Failed]AppSettingsFilename not set, exiting."
+}
+
+$fileContent = Get-Content $ApplicationName -Raw
+
+$fileContent = $fileContent -replace "{{TenantId}}", $TenantId
+$fileContent = $fileContent -replace "{{SubscriptionId}}", $SubscriptionId
+$fileContent = $fileContent -replace "{{ClientId}}", $ClientId
+$fileContent = $fileContent -replace "{{ClientSecret}}", $ClientSecret
+$fileContent = $fileContent -replace "{{ApplicationName}}", $ApplicationName
+
+$fileContent | Out-File $AppSettingsFilename -Force -Encoding utf8
