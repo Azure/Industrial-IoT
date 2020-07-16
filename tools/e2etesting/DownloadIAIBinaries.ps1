f40052e169498768681cd9366a5cbf9a1aadc051
Markus Stuhler <mstuhler@microsoft.com>
2020-07-16 14:53:11 +0200

    Updated IAI script

--- tools/e2etesting/DownloadIAIBinaries.ps1
+++ tools/e2etesting/DownloadIAIBinaries.ps1
@@ -1,6 +1,9 @@
 param(
     [string]
-    $IAIBlobUrl,
+    $IAIStorageAccountName,
+
+    [string]
+    $IAIStorageAccountContainerName,
 
     [string]
     $IAIVersion,
@@ -9,8 +12,12 @@ param(
     $IAILocalFolder
 )
 
-if (!$IAIBlobUrl) {
-    Write-Host "##vso[task.complete result=Failed]IAIBlobUrl not set, exiting."
+if (!$IAIStorageAccountName) {
+    Write-Host "##vso[task.complete result=Failed]IAIStorageAccountName not set, exiting."
+}
+
+if (!$IAIStorageAccountContainerName) {
+    Write-Host "##vso[task.complete result=Failed]IAIStorageAccountContainerName not set, exiting."
 }
 
 if (!$IAILocalFolder) {
@@ -19,17 +26,47 @@ if (!$IAILocalFolder) {
 }
 
 if (!$IAIVersion) {
-    Write-Host "##vso[task.complete result=Failed]IAIVersion not set, exiting."
+    Write-Host "IAIVersion not set, using latest..."
+    $IAIVersion = "latest"
 }
 
-$iaiBinaryFilename = "Microsoft.Azure.IIoT.Deployment.exe"
-$iaiUrlPostfix = "/win-x64/" + $iaiBinaryFilename
+Import-Module Azure.Storage -Force
 
-$iaiFullUrl = $IAIBlobUrl.TrimEnd('/') + "/" + $IAIVersion + "/" + $iaiUrlPostfix.TrimStart('/')
-$iaiLocalFilename = [System.IO.Path]::Combine($IAILocalFolder, $iaiBinaryFilename)
+$context = New-AzureStorageContext -StorageAccountName azureiiot -Anonymous
+
+if (!$context) {
+    Write-Host "##vso[task.complete result=Failed]Could not retrieve storage context with name '$($IAIStorageAccountName), exiting.'"
+}
+
+$blobObjects = Get-AzureStorageBlob -Container $IAIStorageAccountContainerName -Context $context
+
+if (!$blobObjects) {
+    Write-Host "##vso[task.complete result=Failed]Could not get blob contents in storage account '$($IAIStorageAccountName), container '$($IAIStorageAccountContainerName)', exiting.'"
+}
+
+$blobObjects = $blobObjects | ?{ $_.Name.StartsWith("master") -and $_.Name.EndsWith(".exe") }
+
+if ($IAIVersion -eq "latest") {
+    $blobObject = $blobObjects | sort -Descending Name | select -First 1
+} else {
+    $blobObject = $blobObject | ?{ $_.name -contains $IAIVersion } | select -First 1
+}
+
+$version = $blobObject.Name.Split("/") | select -Skip 1 -First 1
+
+if (!$blobObject) {
+    Write-Host "##vso[task.complete result=Failed]Could not find blob object with selected version '$($IAIVersion)', exiting..."
+}
+
+$baseUrl = $context.BlobEndpoint
+$iaiFullUrl = $baseUrl.TrimEnd("/") + "/" + $IAIStorageAccountContainerName + "/" + $blobObject.Name.TrimStart("/")
+$fileName = $blobObject.Name.Split("/") | select -Last 1
+
+$iaiLocalFilename = [System.IO.Path]::Combine($IAILocalFolder, $fileName)
 
 Write-Host "##[group]Downloading IAI binaries..."
 Write-Host "IAI binary download"
+Write-Host "  Version: $($version)"
 Write-Host "  Source: $($iaiFullUrl)"
 Write-Host "  Target: $($iaiLocalFilename)"
 
