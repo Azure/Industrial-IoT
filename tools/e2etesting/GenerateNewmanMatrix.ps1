e43d0e836b8832720b65f7b1836cf6b9ed82f092
Markus Stuhler <mstuhler@microsoft.com>
2020-07-23 11:52:42 +0200

    Updated newman commands

--- tools/e2etesting/GenerateNewmanMatrix.ps1
+++ tools/e2etesting/GenerateNewmanMatrix.ps1
@@ -10,15 +10,8 @@ $collectionFiles = Get-ChildItem -Path $Path -Recurse -Filter "*.postman_collect
 $jobs = @{}
 
 foreach ($collectionFile in $collectionFiles) {
-    $environmentFile = $collectionFile.FullName.Replace("postman_collection", "postman_environment")
-
-    if (!(Test-Path $environmentFile)) {
-        Write-Host "Could not locate environment file '$($environmentFile)' for collection '$($collectionFile.FullName)', skipping..."
-        continue
-    }
-
-    Write-Host "Adding collection '$($collectionFile.FullName)' with environment '$($environmentFile)' to job list..."
-    $jobs.Add($collectionFile.Name, @{ "collectionFile" = $collectionFile.Fullname; "environmentFile" = $environmentFile })
+    Write-Host "Adding collection '$($collectionFile.FullName)'  to job list..."
+    $jobs.Add($collectionFile.Name, @{ "collectionFile" = $collectionFile.Fullname;  })
 }
 
 Write-Host ("##vso[task.setVariable variable=postmanJobsMatrix;isOutput=true] {0}" -f ($jobs | ConvertTo-Json -Compress))
