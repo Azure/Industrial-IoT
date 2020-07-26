eb4ffe734a5da8a910d1ec64fa3ba62f3ad88a9d
Luis Cantero <lcantero@microsoft.com>
2020-07-26 15:50:12 +0200

    Prefer PCS_MSI_TENANT for local debugging

--- common/src/Microsoft.Azure.IIoT.Auth.ActiveDirectory/src/Extensions/ConfigurationEx.cs
+++ common/src/Microsoft.Azure.IIoT.Auth.ActiveDirectory/src/Extensions/ConfigurationEx.cs
@@ -198,10 +198,10 @@ namespace Microsoft.Extensions.Configuration {
                 }
                 catch (Exception ex) {
                     throw new InvalidConfigurationException(
-                        "A keyvault uri was provided could not access keyvault at the address. " +
-                        "If you want to read configuration from keyvault, make sure " +
-                        "the keyvault is reachable, the required permissions are configured " +
-                        "on keyvault and authentication provider information is available. " +
+                        "Could not access the provided keyvault URI. " +
+                        "If you want to read configuration from the keyvault, make sure " +
+                        "it is reachable, the required permissions are configured " +
+                        "and authentication provider information is available. " +
                         "Sign into Visual Studio or Azure CLI on this machine and try again.", ex);
                 }
                 if (!lazyLoad) {
