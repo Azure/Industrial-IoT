3b188686753e984070482129c1c7e2c3445b904c
Markus Stuhler <mstuhler@microsoft.com>
2020-07-28 16:37:27 +0200

    Bugfix

--- tools/e2etesting/SetKeyVaultPermissions.ps1
+++ tools/e2etesting/SetKeyVaultPermissions.ps1
@@ -7,7 +7,7 @@ $keyVaultName = $KeyVaultUrl.Split('.')[0].Split('/')[2]
 $upn = (Get-AzContext).Account.Id
 
 Write-Host "Adding List,Get-Permissions for secrets of vault '$($keyVaultName)' for user '$($upn)'"
-Set-AzKeyVaultAccessPolicy -VaultName '$keyVaultName' -ResourceGroupName '$ResourceGroupName' -UserPrincipalName '$upn' -PermissionsToSecrets get,list
+Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -ResourceGroupName $ResourceGroupName -UserPrincipalName $upn -PermissionsToSecrets get,list
 
 Write-Host "Setting output-variable 'KeyVaultName' to '$($keyVaultName)'"
 Write-Host "##vso[task.setvariable variable=KeyVaultName;isOutput=true]$keyVaultName"
