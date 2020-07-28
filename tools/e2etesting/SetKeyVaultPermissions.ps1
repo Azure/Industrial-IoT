945d8ba635b4dd3583060b6f2eedc67420424081
Markus Stuhler <mstuhler@microsoft.com>
2020-07-28 15:08:00 +0200

    Added KeyVault-Script

---
+++ tools/e2etesting/SetKeyVaultPermissions.ps1
@@ -0,0 +1,13 @@
+Param(
+    $KeyVaultUrl,
+    $ResourceGroupName
+)
+
+$keyVaultName = $KeyVaultUrl.Split('.')[0].Split('/')[2]
+$upn = (Get-AzContext).Account.Id
+
+Write-Host "Adding List,Get-Permissions for secrets of vault $($keyVaultName) for user $($upn)"
+Set-AzureRmKeyVaultAccessPolicy -VaultName '$keyVaultName' -ResourceGroupName '$ResourceGroupName' -UserPrincipalName '$upn' -PermissionsToSecrets get,list
+
+Write-Host "Setting output-variable 'KeyVaultName' to '$($keyVaultName)'"
+Write-Host "##vso[task.setvariable variable=KeyVaultName;isOutput=true]$keyVaultName"
