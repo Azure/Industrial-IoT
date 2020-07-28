55652c39e79a5a02628403e35c8eb694d22e17e0
Markus Stuhler <mstuhler@microsoft.com>
2020-07-28 17:43:58 +0200

    Testing

--- tools/e2etesting/SetKeyVaultPermissions.ps1
+++ tools/e2etesting/SetKeyVaultPermissions.ps1
@@ -4,10 +4,14 @@ Param(
 )
 
 $keyVaultName = $KeyVaultUrl.Split('.')[0].Split('/')[2]
-$upn = (Get-AzContext).Account.Id
+
+$vm = Get-AzVm -Name $env:COMPUTERNAME
+$objectId = $vm.Identity.PrincipalId
+
+Write-Host "Using ObjectId '$($objectId)' ($($env:COMPUTERNAME)) for access permissions."
 
 Write-Host "Adding List,Get-Permissions for secrets of vault '$($keyVaultName)' for user '$($upn)'"
-Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -ResourceGroupName $ResourceGroupName -UserPrincipalName $upn -PermissionsToSecrets get,list
+Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -ResourceGroupName $ResourceGroupName -ObjectId $objectId -PermissionsToSecrets get,list
 
 Write-Host "Setting output-variable 'KeyVaultName' to '$($keyVaultName)'"
 Write-Host "##vso[task.setvariable variable=KeyVaultName;isOutput=true]$keyVaultName"
