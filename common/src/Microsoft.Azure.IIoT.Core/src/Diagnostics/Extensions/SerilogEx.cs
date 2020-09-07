ac30d0923ceb403a6b270cc75576ae8c450016f8
Monika Varshney <9294861+monikavar@users.noreply.github.com>
2020-09-07 18:27:37 +0200

    adding date to log entries (#700)
    * adding date to log entries
    
    * adding UTC timestamp

--- common/src/Microsoft.Azure.IIoT.Core/src/Diagnostics/Extensions/SerilogEx.cs
+++ common/src/Microsoft.Azure.IIoT.Core/src/Diagnostics/Extensions/SerilogEx.cs
@@ -66,6 +66,6 @@ namespace Serilog {
         }
 
         private const string kDefaultTemplate =
-            "[{Timestamp:HH:mm:ss} {Level:u3} {SourceContext:lj}] {Message:lj} {NewLine}{Exception}";
+            "[{Timestamp:u} {Level:u3} {SourceContext:lj}] {Message:lj} {NewLine}{Exception}";
     }
 }
