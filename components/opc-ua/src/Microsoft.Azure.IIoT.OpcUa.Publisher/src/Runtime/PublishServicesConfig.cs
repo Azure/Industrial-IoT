8855fe55caf0840cfad74a25056ede0b10dc0c5a
Luis Cantero <lcantero@microsoft.com>
2020-09-17 23:52:35 +0200

    Fix tests

--- components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Publisher/src/Runtime/PublishServicesConfig.cs
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Publisher/src/Runtime/PublishServicesConfig.cs
@@ -71,7 +71,15 @@ namespace Microsoft.Azure.IIoT.OpcUa.Publisher.Runtime {
             get {
                 var queueSize = GetIntOrDefault(kDefaultMaxEgressMessageQueue,
                     () => GetIntOrDefault(PcsVariable.PCS_DEFAULT_PUBLISH_MAX_EGRESS_MESSAGE_QUEUE,
-                    () => _DefaultMaxEgressMessageQueue));
+                    () => -1));
+
+                if (queueSize == -1) {
+                    // Fallback to deprecated option,
+                    // use PCS_DEFAULT_PUBLISH_MAX_EGRESS_MESSAGE_QUEUE instead.
+                    queueSize = GetIntOrDefault(kDefaultMaxEgressMessageQueue,
+                        () => GetIntOrDefault(PcsVariable.PCS_DEFAULT_PUBLISH_MAX_OUTGRESS_MESSAGES,
+                        () => _DefaultMaxEgressMessageQueue));
+                }
 
                 if (queueSize > 1 && queueSize <= 25000) {
                     //ToDo: This behavior should be logged.
