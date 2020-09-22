7e7180bea24b66dc2af5bb2457957f964c27bdf4
Dan Silver <dansil@microsoft.com>
2020-09-22 16:22:13 -0700

    rename arm deployment var

--- tools/e2etesting/DeploySimulation.ps1
+++ tools/e2etesting/DeploySimulation.ps1
@@ -34,17 +34,17 @@ Write-Host "Created DPS"
 Write-Host "Creating MSI for edge VM identity"
 
 $template = [System.IO.Path]::Combine($templateDir, "azuredeploy.managedidentity.json")
-$prereqsDeployment = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $template
-if ($prereqsDeployment.ProvisioningState -ne "Succeeded") {
-    Write-Error "Deployment $($prereqsDeployment.ProvisioningState)." -ErrorAction Stop
+$msiDeployment = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $template
+if ($msiDeployment.ProvisioningState -ne "Succeeded") {
+    Write-Error "Deployment $($msiDeployment.ProvisioningState)." -ErrorAction Stop
 }
 
-Write-Host "Created MSI $($prereqsDeployment.Parameters.managedIdentityName.Value) with resource id $($prereqsDeployment.Outputs.managedIdentityResourceId.Value)"
+Write-Host "Created MSI $($msiDeployment.Parameters.managedIdentityName.Value) with resource id $($msiDeployment.Outputs.managedIdentityResourceId.Value)"
 
 # Configure the keyvault
 # Allow the MSI to access keyvault
 # https://github.com/Azure/azure-powershell/issues/10029 for why -BypassObjectIdValidation is needed
-Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -ObjectId $prereqsDeployment.Outputs.managedIdentityPrincipalId.Value -PermissionsToSecrets get,list,set,delete -PermissionsToKeys get,list,sign,unwrapKey,wrapKey,create -PermissionsToCertificates get,list,update,create,import -BypassObjectIdValidation
+Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -ObjectId $msiDeployment.Outputs.managedIdentityPrincipalId.Value -PermissionsToSecrets get,list,set,delete -PermissionsToKeys get,list,sign,unwrapKey,wrapKey,create -PermissionsToCertificates get,list,update,create,import -BypassObjectIdValidation
 Write-Host "Key vault set to allow MSI full access"
 
 # Allow the keyvault to be used in ARM deployments
@@ -54,7 +54,7 @@ Write-Host "Key vault configured to be used in ARM deployments"
 # Deploy edge and simulation virtual machines
 $templateParameters = @{
     "keyVaultName" = $keyVaultName
-	"managedIdentityResourceId" = $prereqsDeployment.Outputs.managedIdentityResourceId.Value
+	"managedIdentityResourceId" = $msiDeployment.Outputs.managedIdentityResourceId.Value
     "numberOfLinuxGateways" = 1
     "edgePassword" = [System.Web.Security.Membership]::GeneratePassword(15, 5)
     "branchName" = $branchName
