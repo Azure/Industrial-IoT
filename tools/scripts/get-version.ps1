240b56a1857dcb8b3b589c7ccce019124b34d5c7
Martin Regen <mregen@microsoft.com>
2020-09-22 16:30:14 +0200

    fix PR feedback

--- tools/scripts/get-version.ps1
+++ tools/scripts/get-version.ps1
@@ -9,7 +9,7 @@
 
 try {
     # Try install tool
-    & dotnet @("tool", "install", "--tool-path", "./tools", "--framework", "netcoreapp3.1", "nbgv") 2>&1 # | Out-Null
+    & dotnet @("tool", "install", "--tool-path", "./tools", "--framework", "netcoreapp3.1", "nbgv") 2>&1 
 
     $props = (& ./tools/nbgv  @("get-version", "-f", "json")) | ConvertFrom-Json
     if ($LastExitCode -ne 0) {
