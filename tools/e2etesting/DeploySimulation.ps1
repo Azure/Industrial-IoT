2e9da4f242a047430e6e8c1cdcc70128a1b1886b
dan-silver <dannysilver3@gmail.com>
2020-09-24 12:27:11 -0700

    Deploy simulation and gateway servers as part of test pipeline (#727)
    * Deploy simulation as part of test pipeline
    
    * rename ps1 file
    
    * rename arm deployment var
    
    * disable PR triggers
    
    * Add AzureSubscription pipeline variable
    
    * Remove extra permissions

---
+++ tools/e2etesting/DeploySimulation.ps1
@@ -0,0 +1,71 @@
+Param(
+    $keyVaultName,
+    $resourceGroupName,
+    $branchName
+)
+
+$branchName = $branchName.Replace('refs/heads/', '')
+$templateDir = [System.IO.Path]::Combine($PSScriptRoot, "../../deploy/templates") 
+
+# Get IoT Hub
+$iotHub = Get-AzIotHub -ResourceGroupName $resourceGroupName
+$ioTHubConnString = (Get-AzIotHubConnectionString -ResourceGroupName $resourceGroupName -KeyName iothubowner -Name $iotHub.Name).PrimaryConnectionString
+
+# Create DPS
+Write-Host "Creating device provisioning service"
+
+$templateParameters = @{
+    "dpsIotHubHostName" = $iotHub.Properties.HostName
+    "dpsIotHubConnectionString" = $ioTHubConnString
+    "dpsIotHubLocation" = $iotHub.Location
+    "keyVaultName" = $keyVaultName
+    "branchName" = $branchName
+}
+
+$template = [System.IO.Path]::Combine($templateDir, "azuredeploy.deviceprovisioning.json")
+$dpsDeployment = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $template -TemplateParameterObject $templateParameters
+if ($dpsDeployment.ProvisioningState -ne "Succeeded") {
+    Write-Error "Deployment $($dpsDeployment.ProvisioningState)." -ErrorAction Stop
+}
+
+Write-Host "Created DPS"
+
+# Create MSI for edge
+Write-Host "Creating MSI for edge VM identity"
+
+$template = [System.IO.Path]::Combine($templateDir, "azuredeploy.managedidentity.json")
+$msiDeployment = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $template
+if ($msiDeployment.ProvisioningState -ne "Succeeded") {
+    Write-Error "Deployment $($msiDeployment.ProvisioningState)." -ErrorAction Stop
+}
+
+Write-Host "Created MSI $($msiDeployment.Parameters.managedIdentityName.Value) with resource id $($msiDeployment.Outputs.managedIdentityResourceId.Value)"
+
+# Configure the keyvault
+# Allow the MSI to access keyvault
+# https://github.com/Azure/azure-powershell/issues/10029 for why -BypassObjectIdValidation is needed
+Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -ObjectId $msiDeployment.Outputs.managedIdentityPrincipalId.Value -PermissionsToSecrets get,list -BypassObjectIdValidation
+Write-Host "Key vault set to allow MSI full access"
+
+# Allow the keyvault to be used in ARM deployments
+Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -EnabledForTemplateDeployment
+Write-Host "Key vault configured to be used in ARM deployments"
+
+# Deploy edge and simulation virtual machines
+$templateParameters = @{
+    "keyVaultName" = $keyVaultName
+    "managedIdentityResourceId" = $msiDeployment.Outputs.managedIdentityResourceId.Value
+    "numberOfLinuxGateways" = 1
+    "edgePassword" = [System.Web.Security.Membership]::GeneratePassword(15, 5)
+    "branchName" = $branchName
+}
+
+Write-Host "Preparing to deploy azuredeploy.simulation.json"
+
+$template = [System.IO.Path]::Combine($templateDir, "azuredeploy.simulation.json")
+$simulationDeployment = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $template -TemplateParameterObject $templateParameters
+if ($simulationDeployment.ProvisioningState -ne "Succeeded") {
+    Write-Error "Deployment $($simulationDeployment.ProvisioningState)." -ErrorAction Stop
+}
+
+Write-Host "Deployed simulation"
