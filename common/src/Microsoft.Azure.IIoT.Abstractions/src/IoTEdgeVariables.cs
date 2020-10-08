712a072e3f7aa5e89f9cfd9feab696bfa887df60
Karapet Kostandyan <kakostan@microsoft.com>
2020-10-08 16:55:15 +0200

    Removed unused using directives.

--- common/src/Microsoft.Azure.IIoT.Abstractions/src/IoTEdgeVariables.cs
+++ common/src/Microsoft.Azure.IIoT.Abstractions/src/IoTEdgeVariables.cs
@@ -4,9 +4,6 @@
 // ------------------------------------------------------------
 
 namespace Microsoft.Azure.IIoT.Abstractions {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
 
     /// <summary>
     /// Common IoT Edge runtime environment variables.
