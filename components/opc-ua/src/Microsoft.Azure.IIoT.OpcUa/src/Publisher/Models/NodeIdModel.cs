72bede29649073e53c7d38ab7d44c0ea6a1eff16
markusstuhler <34265641+markusstuhler@users.noreply.github.com>
2020-10-30 15:22:11 +0100

    Poc/central publisher config (#762)
    * poc for iot central publisher standalone config
    
    * bugfix overriding PublishingInterval
    
    * reposition standalone publisher config models
    
    * standalone publisher start-up fix
    
    * Removed unncesesary property mappings
    
    * Updated PublishedNodesJobConverter unit tests
    
    * Bugfix and updated tests
    
    * make null checks before other checks
    
    * Reverted change to read display name
    
    * fix display name related test unit
    
    Co-authored-by: Cristian Pogacean <crpogace@microsoft.com>
    Co-authored-by: markusstuhler <mstuhler@microsoft.com>
    Co-authored-by: Martin Regen <mregen@microsoft.com>

---
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa/src/Publisher/Models/NodeIdModel.cs
@@ -0,0 +1,19 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.IIoT.OpcUa.Publisher.Config.Models {
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Node id serialized as object
+    /// </summary>
+    [DataContract]
+    public class NodeIdModel {
+
+        /// <summary> Identifier </summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string Identifier { get; set; }
+    }
+}
