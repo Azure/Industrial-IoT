423e707dbb385d8b50beb61bda5ea324b7e3e418
Alexander Koepke <alexander.koepke@microsoft.com>
2020-11-09 18:28:18 +0100

    use opc-plc based iotedge simulation

--- tools/e2etesting/DeploySimulation.ps1
+++ tools/e2etesting/DeploySimulation.ps1
@@ -1,6 +1,9 @@
 Param(
+    [string]
     $keyVaultName,
+    [string]
     $resourceGroupName,
+    [string]
     $branchName
 )
 
@@ -12,23 +15,23 @@ $iotHub = Get-AzIotHub -ResourceGroupName $resourceGroupName
 $ioTHubConnString = (Get-AzIotHubConnectionString -ResourceGroupName $resourceGroupName -KeyName iothubowner -Name $iotHub.Name).PrimaryConnectionString
 
 # Create DPS
-Write-Host "Creating device provisioning service"
+# Write-Host "Creating device provisioning service"
 
-$templateParameters = @{
-    "dpsIotHubHostName" = $iotHub.Properties.HostName
-    "dpsIotHubConnectionString" = $ioTHubConnString
-    "dpsIotHubLocation" = $iotHub.Location
-    "keyVaultName" = $keyVaultName
-    "branchName" = $branchName
-}
+# $templateParameters = @{
+#     "dpsIotHubHostName" = $iotHub.Properties.HostName
+#     "dpsIotHubConnectionString" = $ioTHubConnString
+#     "dpsIotHubLocation" = $iotHub.Location
+#     "keyVaultName" = $keyVaultName
+#     "branchName" = $branchName
+# }
 
-$template = [System.IO.Path]::Combine($templateDir, "azuredeploy.deviceprovisioning.json")
-$dpsDeployment = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $template -TemplateParameterObject $templateParameters
-if ($dpsDeployment.ProvisioningState -ne "Succeeded") {
-    Write-Error "Deployment $($dpsDeployment.ProvisioningState)." -ErrorAction Stop
-}
+# $template = [System.IO.Path]::Combine($templateDir, "azuredeploy.deviceprovisioning.json")
+# $dpsDeployment = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $template -TemplateParameterObject $templateParameters
+# if ($dpsDeployment.ProvisioningState -ne "Succeeded") {
+#     Write-Error "Deployment $($dpsDeployment.ProvisioningState)." -ErrorAction Stop
+# }
 
-Write-Host "Created DPS"
+# Write-Host "Created DPS"
 
 # Create MSI for edge
 Write-Host "Creating MSI for edge VM identity"
@@ -51,18 +54,46 @@ Write-Host "Key vault set to allow MSI full access"
 Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -EnabledForTemplateDeployment
 Write-Host "Key vault configured to be used in ARM deployments"
 
-# Deploy edge and simulation virtual machines
-$templateParameters = @{
-    "keyVaultName" = $keyVaultName
-    "managedIdentityResourceId" = $msiDeployment.Outputs.managedIdentityResourceId.Value
-    "numberOfLinuxGateways" = 1
-    "edgePassword" = [System.Web.Security.Membership]::GeneratePassword(15, 5)
-    "branchName" = $branchName
+# Register IoT Edge device at IoT Hub
+$testEdgeDevice = "myTestDevice_$(Get-Random)"
+$edgeIdentity = az iot hub device-identity create --device-id $testEdgeDevice --edge-enabled --hub-name $iotHub.Name | ConvertFrom-Json
+if ($edgeIdentity.status -ne "enabled") {
+    Write-Error "Deployment edge identify for test device" -ErrorAction Stop
 }
 
-Write-Host "Preparing to deploy azuredeploy.simulation.json"
+# Read Device Connection String for TestDevice
+$edgeDeviceConnectionString = az iot hub device-identity connection-string show --device-id $edgeIdentity.deviceId --hub-name $iotHub.Name | ConvertFrom-Json
 
-$template = [System.IO.Path]::Combine($templateDir, "azuredeploy.simulation.json")
+# Update Device Twin of IoT Edge device
+$deviceTwin = az iot hub device-twin show --device-id $edgeIdentity.deviceId --hub-name $iotHub.Name | ConvertFrom-Json
+$tags = "" | Select os,__type__
+$tags.os = "Linux"
+$tags.__type__ = "iiotedge"
+$deviceTwin | Add-Member -MemberType NoteProperty -Value $tags -Name "tags"
+$configFileName = New-TemporaryFile
+($deviceTwin | ConvertTo-Json -Depth 10) | Out-File -FilePath $configFileName -Encoding UTF8
+az iot hub device-twin replace --device-id $edgeIdentity.deviceId --hub-name $iotHub.Name --json $configFileName
+
+# Deploy edge and simulation virtual machines
+$templateParameters = @{
+    "factoryName" = "contoso"
+    "vmUsername" = "sandboxuser"
+    "vmPassword" = [System.Web.Security.Membership]::GeneratePassword(15, 5)
+    "edgeVmSize" = "Standard_D2s_v3"
+    "numberOfSimulations" = 18
+    "numberOfSlowNodes" = 1000
+    "slowNodeRate" = 10
+    "slowNodeType" = "uint"
+    "numberOfFastNodes" = 1000
+    "fastNodeRate" = 1
+    "fastNodeType" = "uint"
+    "iotEdgeConnString" = $edgeDeviceConnectionString.connectionString
+    "keyVaultName" = $keyVaultName
+}
+
+Write-Host "Preparing to deploy e2e.edge.and.plc.simulation.json"
+
+$template = [System.IO.Path]::Combine($templateDir, "e2e.edge.and.plc.simulation.json")
 $simulationDeployment = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $template -TemplateParameterObject $templateParameters
 if ($simulationDeployment.ProvisioningState -ne "Succeeded") {
     Write-Error "Deployment $($simulationDeployment.ProvisioningState)." -ErrorAction Stop
