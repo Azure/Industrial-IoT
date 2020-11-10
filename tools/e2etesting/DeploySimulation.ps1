08dbc4db2796a189249f2039227e37eb5ab73f59
Alexander Koepke <alexander.koepke@microsoft.com>
2020-11-10 20:17:23 +0100

    change to opc-plc simulation

--- tools/e2etesting/DeploySimulation.ps1
+++ tools/e2etesting/DeploySimulation.ps1
@@ -4,51 +4,45 @@ Param(
     [string]
     $resourceGroupName,
     [string]
-    $branchName
+    $branchName,
+    [string]
+    $region,
+    [string]
+    $username,
+    [securestring]
+    $password,
+    [string]
+    $tenantId
 )
+
+# Powershell-Az module (which is automatically authorized by Powershell) 
+# don't support IoT Hub Device Identity methods, we ned to use az cli
+if(-Not ([string]::IsNullOrEmpty($username) && [string]::IsNullOrEmpty($password) && [string]::IsNullOrEmpty($tenantId))) {
+    az login --service-principal --username  $username --password $password --tenant $tenantId
+}
 
 $branchName = $branchName.Replace('refs/heads/', '')
 $templateDir = [System.IO.Path]::Combine($PSScriptRoot, "../../deploy/templates") 
 
 # Get IoT Hub
 $iotHub = Get-AzIotHub -ResourceGroupName $resourceGroupName
-$ioTHubConnString = (Get-AzIotHubConnectionString -ResourceGroupName $resourceGroupName -KeyName iothubowner -Name $iotHub.Name).PrimaryConnectionString
 
-# Create DPS
-# Write-Host "Creating device provisioning service"
+# # Create MSI for edge
+# Write-Host "Creating MSI for edge VM identity"
 
-# $templateParameters = @{
-#     "dpsIotHubHostName" = $iotHub.Properties.HostName
-#     "dpsIotHubConnectionString" = $ioTHubConnString
-#     "dpsIotHubLocation" = $iotHub.Location
-#     "keyVaultName" = $keyVaultName
-#     "branchName" = $branchName
+# $template = [System.IO.Path]::Combine($templateDir, "azuredeploy.managedidentity.json")
+# $msiDeployment = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $template
+# if ($msiDeployment.ProvisioningState -ne "Succeeded") {
+#     Write-Error "Deployment $($msiDeployment.ProvisioningState)." -ErrorAction Stop
 # }
 
-# $template = [System.IO.Path]::Combine($templateDir, "azuredeploy.deviceprovisioning.json")
-# $dpsDeployment = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $template -TemplateParameterObject $templateParameters
-# if ($dpsDeployment.ProvisioningState -ne "Succeeded") {
-#     Write-Error "Deployment $($dpsDeployment.ProvisioningState)." -ErrorAction Stop
-# }
+# Write-Host "Created MSI $($msiDeployment.Parameters.managedIdentityName.Value) with resource id $($msiDeployment.Outputs.managedIdentityResourceId.Value)"
 
-# Write-Host "Created DPS"
-
-# Create MSI for edge
-Write-Host "Creating MSI for edge VM identity"
-
-$template = [System.IO.Path]::Combine($templateDir, "azuredeploy.managedidentity.json")
-$msiDeployment = New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $template
-if ($msiDeployment.ProvisioningState -ne "Succeeded") {
-    Write-Error "Deployment $($msiDeployment.ProvisioningState)." -ErrorAction Stop
-}
-
-Write-Host "Created MSI $($msiDeployment.Parameters.managedIdentityName.Value) with resource id $($msiDeployment.Outputs.managedIdentityResourceId.Value)"
-
-# Configure the keyvault
-# Allow the MSI to access keyvault
-# https://github.com/Azure/azure-powershell/issues/10029 for why -BypassObjectIdValidation is needed
-Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -ObjectId $msiDeployment.Outputs.managedIdentityPrincipalId.Value -PermissionsToSecrets get,list -BypassObjectIdValidation
-Write-Host "Key vault set to allow MSI full access"
+# # Configure the keyvault
+# # Allow the MSI to access keyvault
+# # https://github.com/Azure/azure-powershell/issues/10029 for why -BypassObjectIdValidation is needed
+# Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -ObjectId $msiDeployment.Outputs.managedIdentityPrincipalId.Value -PermissionsToSecrets get,list -BypassObjectIdValidation
+# Write-Host "Key vault set to allow MSI full access"
 
 # Allow the keyvault to be used in ARM deployments
 Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -EnabledForTemplateDeployment
@@ -57,8 +51,13 @@ Write-Host "Key vault configured to be used in ARM deployments"
 # Register IoT Edge device at IoT Hub
 $testEdgeDevice = "myTestDevice_$(Get-Random)"
 $edgeIdentity = az iot hub device-identity create --device-id $testEdgeDevice --edge-enabled --hub-name $iotHub.Name | ConvertFrom-Json
-if ($edgeIdentity.status -ne "enabled") {
-    Write-Error "Deployment edge identify for test device" -ErrorAction Stop
+Write-Host "Created edge device identity $($edgeIdentity.deviceId)"
+
+# Store Device Id within KeyVault to be useable by rerun without deployment (USeExisting=true)
+$secretvalue = ConvertTo-SecureString $edgeIdentity.deviceId -AsPlainText -Force
+$secret = Set-AzKeyVaultSecret -VaultName $keyVaultName -Name 'iot-edge-device-id' -SecretValue $secretvalue
+if (-Not $secret.Enabled) {
+    Write-Error "Couldn't store device identity in KeyVault" -ErrorAction Stop
 }
 
 # Read Device Connection String for TestDevice
@@ -72,15 +71,18 @@ $tags.__type__ = "iiotedge"
 $deviceTwin | Add-Member -MemberType NoteProperty -Value $tags -Name "tags"
 $configFileName = New-TemporaryFile
 ($deviceTwin | ConvertTo-Json -Depth 10) | Out-File -FilePath $configFileName -Encoding UTF8
-az iot hub device-twin replace --device-id $edgeIdentity.deviceId --hub-name $iotHub.Name --json $configFileName
+$out = az iot hub device-twin replace --device-id $edgeIdentity.deviceId --hub-name $iotHub.Name --json $configFileName
+Write-Host "Added Industrial IoT tags to Device Twin of edge device"
 
+$vmpassword = -join ((65..90) + (97..122) + (33..38) + (40..47) + (48..57)| Get-Random -Count 20 | % {[char]$_})
+$numberOfSimulations = 18
 # Deploy edge and simulation virtual machines
 $templateParameters = @{
     "factoryName" = "contoso"
     "vmUsername" = "sandboxuser"
-    "vmPassword" = [System.Web.Security.Membership]::GeneratePassword(15, 5)
+    "vmPassword" = $vmpassword
     "edgeVmSize" = "Standard_D2s_v3"
-    "numberOfSimulations" = 18
+    "numberOfSimulations" = $numberOfSimulations
     "numberOfSlowNodes" = 1000
     "slowNodeRate" = 10
     "slowNodeType" = "uint"
@@ -98,5 +100,19 @@ $simulationDeployment = New-AzResourceGroupDeployment -ResourceGroupName $resour
 if ($simulationDeployment.ProvisioningState -ne "Succeeded") {
     Write-Error "Deployment $($simulationDeployment.ProvisioningState)." -ErrorAction Stop
 }
+
+# Generate URLs to access published_nodes.json via HTTP endpoint of plc-simulation.
+$plcSimNames = ""
+for ($i=1; $i -le $numberOfSimulations; $i++) {
+    $plcSimNames += "http://$($simulationDeployment.Outputs.plcPrefix.Value)$($i).$($region).azurecontainer.io/pn.json;"
+}
+
+# Store Url to PLC simulations in KeyVault
+$secretvalue = ConvertTo-SecureString $plcSimNames -AsPlainText -Force
+$secret = Set-AzKeyVaultSecret -VaultName $keyVaultName -Name 'plc-simulation-urls' -SecretValue $secretvalue
+if (-Not $secret.Enabled) {
+    Write-Error "Couldn't store URLs to access PLC simulation in KeyVault" -ErrorAction Stop
+}
+Write-Host "Stored URLs to access PLC simulation in KeyVault"
 
 Write-Host "Deployed simulation"
