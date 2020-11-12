2ff17a94fbeec9f51632ba06e7a0194264d36f26
Alexander Koepke <alexander.koepke@microsoft.com>
2020-11-12 10:04:26 +0100

    Update SetKeyVaultPermissions for ServicePrincipal service connection

--- tools/e2etesting/SetKeyVaultPermissions.ps1
+++ tools/e2etesting/SetKeyVaultPermissions.ps1
@@ -1,8 +1,25 @@
 Param(
+    [string]
     $KeyVaultName,
+    [string]
     $ResourceGroupName,
-    $ObjectId
+    [string]
+    $ServicePrincipalName
 )
 
-Write-Host "Adding List,Get-Permissions for secrets of vault '$($KeyVaultName)' for user '$($upn)'"
-Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName -ObjectId $objectId -PermissionsToSecrets get,list
+$azContext = Get-AzContext
+Write-Host "AccountId: $($azContext.Account.Id)"
+Write-Host "Context:"
+$azContext
+
+if ($ServicePrincipalName) {
+    Write-Host "Adding List,Get-Permissions for secrets of vault '$($KeyVaultName)' for ServicePrincipalName '$($ServicePrincipalName)'"
+    Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName -ServicePrincipalName $ServicePrincipalName -PermissionsToSecrets get,list
+} else {
+    if ($azContext.Account.Id) {
+        Write-Host "Adding List,Get-Permissions for secrets of vault '$($KeyVaultName)' for UserPrincipalName '$($azContext.Account.Id)'"
+        Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName -UserPrincipalName $azContext.Account.Id -PermissionsToSecrets get,list
+    } else {
+        Write-Error "Not logged in" -ErrorAction Stop
+    }
+} 
