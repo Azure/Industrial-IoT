af4bc4d81cee95930823d38fb60be37eb16e73bb
Daniele Colonna <dacol@microsoft.com>
2020-11-23 15:44:01 +0100

    Added new env variable

--- e2e-tests/IIoTPlatform-E2E-Tests/TestConstants.cs
+++ e2e-tests/IIoTPlatform-E2E-Tests/TestConstants.cs
@@ -154,17 +154,17 @@ namespace IIoTPlatform_E2E_Tests {
             /// The connection string of the event-hub compatible endpoint of IoT Hub.
             /// </summary>
             public const string IOTHUB_EVENTHUB_CONNECTIONSTRING = "IOTHUB_EVENTHUB_CONNECTIONSTRING";
-            
+
             /// <summary>
             /// The connection string of the storage account that will be used for checkpointing (when monitoring IoT Hub)
             /// </summary>
             public const string STORAGE_CONNECTIONSTRING = "STORAGE_CONNECTIONSTRING";
-            
+
             /// <summary>
             /// The service base url of the TestEventProcessor
             /// </summary>
             public const string TESTEVENTPROCESSOR_BASEURL = "TESTEVENTPROCESSOR_BASEURL";
-            
+
             /// <summary>
             /// The username to authenticate against the TestEventProcessor (Basic Auth)
             /// </summary>
