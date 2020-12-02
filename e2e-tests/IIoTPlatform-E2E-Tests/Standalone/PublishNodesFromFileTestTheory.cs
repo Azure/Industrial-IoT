7c5113fbe8dc3e99d83beb8ebb754cd9e58d1657
markusstuhler <34265641+markusstuhler@users.noreply.github.com>
2020-12-02 14:31:20 +0100

    Feature/automated testing updated xUnit traits (#943)
    * Updated xUnit traits
    
    * updated dotnet test verbosity
    
    Co-authored-by: Markus Stuhler <mstuhler@microsoft.com>

--- e2e-tests/IIoTPlatform-E2E-Tests/Standalone/PublishNodesFromFileTestTheory.cs
+++ e2e-tests/IIoTPlatform-E2E-Tests/Standalone/PublishNodesFromFileTestTheory.cs
@@ -17,6 +17,7 @@ namespace IIoTPlatform_E2E_Tests.Standalone {
     /// </summary>
     [TestCaseOrderer("IIoTPlatform_E2E_Tests.TestExtensions.TestOrderer", TestConstants.TestAssemblyName)]
     [Collection("IIoT Platform Test Collection")]
+    [Trait(TestConstants.TraitConstants.PublisherModeTraitName, TestConstants.TraitConstants.PublisherModeStandaloneTraitValue)]
     public class PublishNodesFromFileTestTheory {
         private readonly ITestOutputHelper _output;
         private readonly IIoTPlatformTestContext _context;
