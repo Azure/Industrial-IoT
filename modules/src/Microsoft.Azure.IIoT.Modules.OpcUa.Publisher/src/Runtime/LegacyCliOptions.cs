c9d3f47037ba8dffac60bb98aa5d2fb9f1cd144e
Erich Barnstedt <erichb@microsoft.com>
2021-02-23 12:45:04 +0100

    Enable CA-signed app certs for OPC Publisher standalone mode. (#1027)
    * Enable CA-signed app certs for OPC Publisher standalone mode.
    
    * Added application name as CLI parameter as this needs to match the CN field in the cert subject name.
    
    Co-authored-by: Alexander Köpke <koepalex@xdigital.de>

--- modules/src/Microsoft.Azure.IIoT.Modules.OpcUa.Publisher/src/Runtime/LegacyCliOptions.cs
+++ modules/src/Microsoft.Azure.IIoT.Modules.OpcUa.Publisher/src/Runtime/LegacyCliOptions.cs
@@ -81,7 +81,7 @@ namespace Microsoft.Azure.IIoT.Modules.OpcUa.Publisher.Runtime {
                         "event setting of nodes without a skip first event setting.",
                         (bool b) => this[LegacyCliConfigKeys.SkipFirstDefault] = b.ToString() },
 
-                    { "fm|fullfeaturedmessage=", "The full featured mode for messages (all fields filled in)." + 
+                    { "fm|fullfeaturedmessage=", "The full featured mode for messages (all fields filled in)." +
                         "Default is 'true', for legacy compatibility use 'false'",
                         (bool b) => this[LegacyCliConfigKeys.FullFeaturedMessage] = b.ToString() },
 
@@ -131,6 +131,10 @@ namespace Microsoft.Azure.IIoT.Modules.OpcUa.Publisher.Runtime {
                         s => this[LegacyCliConfigKeys.OpcOwnCertStorePath] = s },
                     { "tp|trustedcertstorepath=", "The path of the trusted cert store.",
                         s => this[LegacyCliConfigKeys.OpcTrustedCertStorePath] = s },
+                    { "sn|appcertsubjectname=", "The subject name for the app cert.",
+                        s => this[LegacyCliConfigKeys.OpcApplicationCertificateSubjectName] = s },
+                    { "an|appname=", "The name for the app (used during OPC UA authentication).",
+                        s => this[LegacyCliConfigKeys.OpcApplicationName] = s },
                     { "tt|trustedcertstoretype=", "Legacy - do not use.", _ => {} },
                     { "rp|rejectedcertstorepath=", "The path of the rejected cert store.",
                         s => this[LegacyCliConfigKeys.OpcRejectedCertStorePath] = s },
@@ -213,7 +217,7 @@ namespace Microsoft.Azure.IIoT.Modules.OpcUa.Publisher.Runtime {
 #pragma warning restore 67
 
         /// <summary>
-        /// The batch size 
+        /// The batch size
         /// </summary>
         public int? BatchSize => LegacyCliModel.BatchSize;
 
@@ -228,7 +232,7 @@ namespace Microsoft.Azure.IIoT.Modules.OpcUa.Publisher.Runtime {
         public TimeSpan? DiagnosticsInterval => LegacyCliModel.DiagnosticsInterval;
 
         /// <summary>
-        /// the Maximum (IoT D2C) message size 
+        /// the Maximum (IoT D2C) message size
         /// </summary>
         public int? MaxMessageSize => LegacyCliModel.MaxMessageSize;
 
@@ -287,6 +291,8 @@ namespace Microsoft.Azure.IIoT.Modules.OpcUa.Publisher.Runtime {
                 AutoAcceptUntrustedCertificates = GetValueOrDefault(LegacyCliConfigKeys.AutoAcceptCerts, false),
                 ApplicationCertificateStoreType = GetValueOrDefault(LegacyCliConfigKeys.OpcOwnCertStoreType, "Directory"),
                 ApplicationCertificateStorePath = GetValueOrDefault(LegacyCliConfigKeys.OpcOwnCertStorePath, "pki/own"),
+                ApplicationCertificateSubjectName = GetValueOrDefault(LegacyCliConfigKeys.OpcApplicationCertificateSubjectName, "CN=Microsoft.Azure.IIoT, C=DE, S=Bav, O=Microsoft, DC=localhost"),
+                ApplicationName = GetValueOrDefault(LegacyCliConfigKeys.OpcApplicationName, "Microsoft.Azure.IIoT"),
                 TrustedPeerCertificatesPath = GetValueOrDefault(LegacyCliConfigKeys.OpcTrustedCertStorePath, "pki/trusted"),
                 RejectedCertificateStorePath = GetValueOrDefault(LegacyCliConfigKeys.OpcRejectedCertStorePath, "pki/rejected"),
                 TrustedIssuerCertificatesPath = GetValueOrDefault(LegacyCliConfigKeys.OpcIssuerCertStorePath, "pki/issuer"),
