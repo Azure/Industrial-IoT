f06772b5395e28c425d1b446f8eb1a3d4e9d2d4b
MaidaB <maidabakovic@gmail.com>
2021-03-02 10:18:17 +0100

    Write secrets to launchSettings.json

--- tools/e2etesting/GetSecrets.ps1
+++ tools/e2etesting/GetSecrets.ps1
@@ -1,8 +1,20 @@
-param($KeyVaultName)
+param(
+	[Parameter(Mandatory=$true)]
+    [string] $KeyVaultName
+)
 
-$secrets = Get-AzKeyVaultSecret -VaultName $KeyVaultName
+$confirmation = Read-Host "Do you want to overwrite the IIoTPlatform-E2E-Tests\PropertieslaunchSettings.json file? yes/no"
+
 $values = @{}
 
+# Get the resource group name 
+$resourceGroup = (Get-AzResource -Name mabakovikeyvault).ResourceGroupName
+$values["ApplicationName"] = $resourceGroup
+
+# Get the names of the secrets from the Key Vault
+$secrets = Get-AzKeyVaultSecret -VaultName $KeyVaultName
+
+# Get the values for the secrets 
 foreach ($secret in $secrets) {
 	$secretValueSec = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $secret.Name
 
@@ -16,4 +28,25 @@ foreach ($secret in $secrets) {
 	$values[$secret.Name.ToUpperInvariant().Replace("-", "_")] = $secretValueText
 }
 
-$values | ConvertTo-Json -Depth 10
+# Find the full path of the launchSettings.json file
+$folderName = "e2e-tests"
+$currentPath = (Get-Location).Path
+$path = Get-ChildItem -Path $currentPath $folderName -Recurse 
+while (!$path) {	
+    $currentPath = $currentPath + '\..'
+    $path = Get-ChildItem -Path $currentPath $folderName 
+}
+$settingsFile = $path.FullName + "\IIoTPlatform-E2E-Tests\Properties\launchSettings.json"
+
+# Write the secrets to launchSettings.json 
+$launchSettings = Get-Content $settingsFile  | ConvertFrom-Json
+$launchSettings.profiles.'IIoTPlatform-E2E-Tests'.environmentVariables = [PSCustomObject]($values)
+$json = ConvertTo-Json $launchSettings -Depth 4 
+
+if ($confirmation -eq "yes"){
+	Set-Content -Path $settingsFile $json
+	Write-Host "The file launchSettings.json is successfully updated with the secrets from your key vault."
+}
+else {
+	Write-Host $json
+}
