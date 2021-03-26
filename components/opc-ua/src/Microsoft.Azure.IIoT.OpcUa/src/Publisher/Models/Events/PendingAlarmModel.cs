2d3a754787358b1b196bbe4518549af5a73ebeff
Peter Bryntesson <pbrynt@microsoft.com>
2021-03-26 14:33:10 +0100

    Added copyright text

--- components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa/src/Publisher/Models/Events/PendingAlarmModel.cs
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa/src/Publisher/Models/Events/PendingAlarmModel.cs
@@ -1,4 +1,9 @@
-﻿namespace Microsoft.Azure.IIoT.OpcUa.Publisher.Config.Models.Events {
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.IIoT.OpcUa.Publisher.Config.Models.Events {
     using System;
     using System.Collections.Generic;
     using System.Runtime.Serialization;
