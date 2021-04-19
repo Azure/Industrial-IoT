33ec50ecb81a14cdc5ba1a28025d2238a1791b1c
Maida Bakovic <maidabakovic@gmail.com>
2021-04-19 10:12:43 +0200

    Integrate nested edge to orchestrated pipeline (#1111)
    * add nested edge resources job
    * job dependency

--- tools/e2etesting/NestedEdge/PostNestedEdgeDeployment.ps1
+++ tools/e2etesting/NestedEdge/PostNestedEdgeDeployment.ps1
@@ -30,7 +30,6 @@ if (!$resourceGroup) {
     Write-Error "Could not find Resource Group '$($ResourceGroupName)'."
 }
 
-
 ## Check if IoT Hub exists
 $iotHub = Get-AzIotHub -ResourceGroupName $ResourceGroupName
 
@@ -40,14 +39,20 @@ if ($iotHub.Count -ne 1) {
 
 Write-Host "IoT Hub Name: $($iotHub.Name)"
 
-$keyVault = "e2etestingkeyVault" + $resourceGroup.Tags["TestingResourcesSuffix"]
-Write-Host "Key Vault Name: $($keyVault)"
+## Check if KeyVault exists
+$keyVault = Get-AzKeyVault -ResourceGroupName $ResourceGroupName
+
+if ($keyVault.Count -ne 1) {
+    Write-Error "keyVault could not be automatically selected in Resource Group '$($ResourceGroupName)'."    
+} 
+
+Write-Host "Key Vault Name: $($keyVault.VaultName)"
 
 $deviceName = "L3-1-edge"
 $edgeIdentity = Get-AzIotHubDevice -ResourceGroupName $ResourceGroupName -IotHubName $iotHub.Name -DeviceId $deviceName -ErrorAction SilentlyContinue
 
 Write-Host "Adding/Updating KeyVault-Secret 'iot-edge-device-id' with value '$($edgeIdentity.Id)'..."
-Set-AzKeyVaultSecret -VaultName $keyVault -Name 'iot-edge-device-id' -SecretValue (ConvertTo-SecureString $edgeIdentity.Id -AsPlainText -Force) | Out-Null
+Set-AzKeyVaultSecret -VaultName $keyVault.VaultName -Name 'iot-edge-device-id' -SecretValue (ConvertTo-SecureString $edgeIdentity.Id -AsPlainText -Force) | Out-Null
 
 
 Write-Host "Updating 'os' and '__type__'-Tags in Device Twin..."
@@ -55,11 +60,11 @@ Update-AzIotHubDeviceTwin -ResourceGroupName $ResourceGroupName -IotHubName $iot
 
 $edgeVmUsername = "jbadmin"
 Write-Host "Adding/Updating KeVault-Secret 'iot-edge-vm-username' with value '$($edgeVmUsername)'..."
-Set-AzKeyVaultSecret -VaultName $keyVault -Name 'iot-edge-vm-username' -SecretValue (ConvertTo-SecureString $edgeVmUsername -AsPlainText -Force) | Out-Null
+Set-AzKeyVaultSecret -VaultName $keyVault.VaultName -Name 'iot-edge-vm-username' -SecretValue (ConvertTo-SecureString $edgeVmUsername -AsPlainText -Force) | Out-Null
 
 
 $fqdn = (Get-AzPublicIpAddress -ResourceGroupName ($ResourceGroupName + "-RG-jumpbox")).DnsSettings.Fqdn
 Write-Host "Adding/Updating KeyVault-Secret 'iot-edge-device-dnsname' with value '$($fqdn)'..."
-Set-AzKeyVaultSecret -VaultName $keyVault -Name 'iot-edge-device-dnsname' -SecretValue (ConvertTo-SecureString $fqdn -AsPlainText -Force) | Out-Null
+Set-AzKeyVaultSecret -VaultName $keyVault.VaultName -Name 'iot-edge-device-dnsname' -SecretValue (ConvertTo-SecureString $fqdn -AsPlainText -Force) | Out-Null
 
 Write-Host "Deployment finished."
