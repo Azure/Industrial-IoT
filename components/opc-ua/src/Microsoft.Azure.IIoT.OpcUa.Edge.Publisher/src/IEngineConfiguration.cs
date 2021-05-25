ffaac10b379be3341b1ebad47fbe997d3b5e61f2
Karapet Kostandyan <kakostan@microsoft.com>
2021-05-25 15:42:00 +0200

    Increased outgress queue size from 200 to 4096.
    Ported part of changes present in #671.

--- components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Edge.Publisher/src/IEngineConfiguration.cs
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Edge.Publisher/src/IEngineConfiguration.cs
@@ -22,7 +22,7 @@ namespace Microsoft.Azure.IIoT.OpcUa.Publisher {
         TimeSpan? BatchTriggerInterval { get; }
 
         /// <summary>
-        /// Maximum mesage size for the encoded messages 
+        /// Maximum mesage size for the encoded messages
         /// typically the IoT Hub's mas D2C message size
         /// </summary>
         int? MaxMessageSize { get; }
@@ -33,9 +33,8 @@ namespace Microsoft.Azure.IIoT.OpcUa.Publisher {
         TimeSpan? DiagnosticsInterval { get; }
 
         /// <summary>
-        /// Define the maximum number of messages in outgress buffer, 
-        /// Default: 200 messages with 256KB ends 
-        /// up in 50 MB memory consumed
+        /// Define the maximum number of messages in outgress buffer,
+        /// Default: 4096 messages with 256KB ends up in 1 GB memory consumed.
         /// </summary>
         int? MaxOutgressMessages { get; }
     }
