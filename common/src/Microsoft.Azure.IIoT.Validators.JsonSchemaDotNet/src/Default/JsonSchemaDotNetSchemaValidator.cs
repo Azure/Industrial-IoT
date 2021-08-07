47c6f9d8af2192afed9928f93258400757809cdf
Bill Berry <wberry@microsoft.com>
2021-08-06 18:15:18 -0700

    Revert version change to sln file, address LGTM automated review feedback.

--- common/src/Microsoft.Azure.IIoT.Validators.JsonSchemaDotNet/src/Default/JsonSchemaDotNetSchemaValidator.cs
+++ common/src/Microsoft.Azure.IIoT.Validators.JsonSchemaDotNet/src/Default/JsonSchemaDotNetSchemaValidator.cs
@@ -22,56 +22,50 @@
                 throw new ArgumentNullException(nameof(schemaReader));
             }
 
-            using (var validationDataStream = new MemoryStream(jsonBuffer)) {
-                using (var validationReader = new StreamReader(validationDataStream)) {
+            var schema = JsonSchema.FromText(schemaReader.ReadToEnd());
 
-                    var schemaString = schemaReader.ReadToEnd();
-                    var schema = JsonSchema.FromText(schemaString);
+            // Use "Basic" output as that will only generate 
+            // two levels of validation results (Root and one level down)
+            // see the API documentation for Json Everything for further details
+            // https://gregsdennis.github.io/json-everything/api/Json.Schema.OutputFormat.html
+            var validationOptions = new ValidationOptions() {
+                OutputFormat = OutputFormat.Basic,
+            };
 
-                    // Use "Basic" output as that will only generate 
-                    // two levels of validation results (Root and one level down)
-                    // see the API documentation for Json Everything for further details
-                    // https://gregsdennis.github.io/json-everything/api/Json.Schema.OutputFormat.html
-                    var validationOptions = new ValidationOptions() {
-                        OutputFormat = OutputFormat.Basic,
-                    };
+            // Run validation ensuring that trailing commas are supported
+            // as it appears trailing commas have been allowable in the 
+            // configuration files for some time. 
+            var validationResults =  schema.Validate(
+                                        JsonDocument.Parse(
+                                        Encoding.UTF8.GetString(jsonBuffer),
+                                        new JsonDocumentOptions() { AllowTrailingCommas = true, }
+                                        ).RootElement,
+                                        validationOptions);
 
-                    // Run validation ensuring that trailing commas are supported
-                    // as it appears trailing commas have been allowable in the 
-                    // configuration files for some time. 
-                    var validationResults =  schema.Validate(
-                                                JsonDocument.Parse(
-                                                validationDataStream.ReadAsString(Encoding.UTF8),
-                                                new JsonDocumentOptions() { AllowTrailingCommas = true, }
-                                                ).RootElement,
-                                                validationOptions);
+            var jsonSchemaValidationResultCollection = new List<JsonSchemaValidationResult>();
 
-                    var jsonSchemaValidationResultCollection = new List<JsonSchemaValidationResult>();
+            // Copy and flatten validation result to generic json validation result objects
+            // as we don't want to bleed JsonSchema.Net lib objects outside of this class.
 
-                    // Copy and flatten validation result to generic json validation result objects
-                    // as we don't want to bleed JsonSchema.Net lib objects outside of this class.
+            // Add the top level error message before iterating the nested errors to add to the collection. 
+            jsonSchemaValidationResultCollection.Add(
+                new JsonSchemaValidationResult(
+                    validationResults.IsValid,
+                    validationResults.Message,
+                    validationResults.SchemaLocation.ToString(),
+                    validationResults.InstanceLocation.ToString()));
 
-                    // Add the top level error message before iterating the nested errors to add to the collection. 
-                    jsonSchemaValidationResultCollection.Add(
-                        new JsonSchemaValidationResult(
-                            validationResults.IsValid,
-                            validationResults.Message,
-                            validationResults.SchemaLocation.ToString(),
-                            validationResults.InstanceLocation.ToString()));
+            // Iterate the nested errors and push them into the error collection
+            jsonSchemaValidationResultCollection.AddRange(
+                validationResults.NestedResults.Select(nr =>
+                    new JsonSchemaValidationResult(
+                        nr.IsValid, 
+                        nr.Message, 
+                        nr.SchemaLocation.ToString(), 
+                        nr.InstanceLocation.ToString())
+            ));
 
-                    // Iterate the nested errors and push them into the error collection
-                    jsonSchemaValidationResultCollection.AddRange(
-                        validationResults.NestedResults.Select(nr =>
-                            new JsonSchemaValidationResult(
-                                nr.IsValid, 
-                                nr.Message, 
-                                nr.SchemaLocation.ToString(), 
-                                nr.InstanceLocation.ToString())
-                    ));
-
-                    return jsonSchemaValidationResultCollection;
-                }
-            }
+            return jsonSchemaValidationResultCollection;
         }
     }
 }
