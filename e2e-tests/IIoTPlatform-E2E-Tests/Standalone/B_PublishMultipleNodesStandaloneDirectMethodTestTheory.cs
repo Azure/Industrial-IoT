243a5cb6d6d3b3a6b48595b5178e761ecad390f5
Daniele Colonna <dacol@microsoft.com>
2021-12-03 16:46:06 +0100

    start adding E2E test infrastructure for dm (#1486)
    * add deploment o publisher 2.5.5
    
    * fix deployment for publisher 2.5.5
    
    * fix image path
    
    * add skeleton for dm test
    
    * removed launchsettings.json
    
    * restored empty lauchsetiings.json file
    
    * add deploment o publisher 2.5.5
    
    * fix deployment for publisher 2.5.5
    
    * fix image path
    
    * add skeleton for dm test
    
    * removed launchsettings.json
    
    * restored empty lauchsetiings.json file
    
    * removed MessagingMode for deployment of publisher 2.5.5
    
    * fix pr comments
    
    * fix
    
    Co-authored-by: Karapet Kostandyan <kakostan@microsoft.com>

---
+++ e2e-tests/IIoTPlatform-E2E-Tests/Standalone/B_PublishMultipleNodesStandaloneDirectMethodTestTheory.cs
@@ -0,0 +1,106 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace IIoTPlatform_E2E_Tests.Standalone {
+    using IIoTPlatform_E2E_Tests.Deploy;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using TestExtensions;
+    using TestModels;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// The test theory using different (ordered) test cases to go thru all required steps of publishing OPC UA node
+    /// </summary>
+    [TestCaseOrderer(TestCaseOrderer.FullName, TestConstants.TestAssemblyName)]
+    [Collection("IIoT Standalone Direct Methods Test Collection")]
+    [Trait(TestConstants.TraitConstants.PublisherModeTraitName, TestConstants.TraitConstants.PublisherModeStandaloneTraitValue)]
+    public class B_PublishMultipleNodesStandaloneDirectMethodTestTheory {
+
+        private readonly ITestOutputHelper _output;
+        private readonly IIoTMultipleNodesTestContext _context;
+
+        public B_PublishMultipleNodesStandaloneDirectMethodTestTheory(
+            ITestOutputHelper output,
+            IIoTMultipleNodesTestContext context
+        ) {
+            _output = output ?? throw new ArgumentNullException(nameof(output));
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _context.OutputHelper = _output;
+        }
+
+        // the test case for now are just empty container that deploy publisher resource
+        // when direct methods will be implemented, also the test should be implemented.
+
+        [Theory]
+        [InlineData(MessagingMode.Samples)]
+        [InlineData(MessagingMode.PubSub)]
+        async Task SubscribeUnsubscribeDirectMethodTest(MessagingMode messagingMode) {
+            var ioTHubEdgeBaseDeployment = new IoTHubEdgeBaseDeployment(_context);
+            var ioTHubPublisherDeployment = new IoTHubPublisherDeployment(_context, messagingMode);
+
+            var cts = new CancellationTokenSource(TestConstants.MaxTestTimeoutMilliseconds);
+
+            // Make sure that there is no active monitoring.
+            await TestHelper.StopMonitoringIncomingMessagesAsync(_context, cts.Token);
+
+            // Create base edge deployment.
+            var baseDeploymentResult = await ioTHubEdgeBaseDeployment.CreateOrUpdateLayeredDeploymentAsync(cts.Token);
+            Assert.True(baseDeploymentResult, "Failed to create/update new edge base deployment.");
+            _output.WriteLine("Created/Updated new edge base deployment.");
+
+            // Create layered edge deployment.
+            var layeredDeploymentResult = await ioTHubPublisherDeployment.CreateOrUpdateLayeredDeploymentAsync(cts.Token);
+            Assert.True(layeredDeploymentResult, "Failed to create/update layered deployment for publisher module.");
+            _output.WriteLine("Created/Updated layered deployment for publisher module.");
+
+            // We will wait for module to be deployed.
+            var exception = Record.Exception(() => _context.RegistryHelper.WaitForIIoTModulesConnectedAsync(
+                _context.DeviceConfig.DeviceId,
+                cts.Token,
+                new string[] { "publisher_standalone" }
+            ).GetAwaiter().GetResult());
+            Assert.Null(exception);
+        }
+
+
+        [Fact]
+        async Task SubscribeUnsubscribeDirectMethodLegacyPublisherTest() {
+            var ioTHubEdgeBaseDeployment = new IoTHubEdgeBaseDeployment(_context);
+            var ioTHubLegacyPublisherDeployment = new IoTHubLegacyPublisherDeployments(_context);
+
+            var cts = new CancellationTokenSource(TestConstants.MaxTestTimeoutMilliseconds);
+
+            // Make sure that there is no active monitoring.
+            await TestHelper.StopMonitoringIncomingMessagesAsync(_context, cts.Token);
+
+            // Create base edge deployment.
+            var baseDeploymentResult = await ioTHubEdgeBaseDeployment.CreateOrUpdateLayeredDeploymentAsync(cts.Token);
+            Assert.True(baseDeploymentResult, "Failed to create/update new edge base deployment.");
+            _output.WriteLine("Created/Updated new edge base deployment.");
+
+            // Create layered edge deployment.
+            var layeredDeploymentResult1 = await ioTHubLegacyPublisherDeployment.CreateOrUpdateLayeredDeploymentAsync(cts.Token);
+            Assert.True(layeredDeploymentResult1, "Failed to create/update layered deployment for legacy publisher module.");
+            _output.WriteLine("Created/Updated layered deployment for legacy publisher module.");
+
+            // Wait some time till the updated pn.json is reflected.
+            await Task.Delay(TestConstants.DefaultTimeoutInMilliseconds);
+
+            // We will wait for module to be deployed.
+            var exception = Record.Exception(() => _context.RegistryHelper.WaitForIIoTModulesConnectedAsync(
+                _context.DeviceConfig.DeviceId,
+                cts.Token,
+                new string[] { "publisher_standalone_legacy" }
+            ).GetAwaiter().GetResult());
+            Assert.Null(exception);
+        }
+
+    }
+}
---
+++ e2e-tests/IIoTPlatform-E2E-Tests/deploy/IoTHubLegacyPublisherDeployments.cs
@@ -0,0 +1,82 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace IIoTPlatform_E2E_Tests.Deploy {
+    using System;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+    using TestExtensions;
+
+    public sealed class IoTHubLegacyPublisherDeployments : DeploymentConfiguration {
+
+        /// <summary>
+        /// Create deployment
+        /// </summary>
+        /// <param name="context"></param>
+        public IoTHubLegacyPublisherDeployments(IIoTPlatformTestContext context) : base(context) {
+        }
+
+        /// <inheritdoc />
+        protected override int Priority => 2;
+
+        /// <inheritdoc />
+        protected override string DeploymentName => kDeploymentName + $"{DateTime.UtcNow.Ticks}";
+
+        protected override string TargetCondition => kTargetCondition;
+
+        /// <inheritdoc />
+        protected override IDictionary<string, IDictionary<string, object>> CreateDeploymentModules() {
+            var registryCredentials = "";
+
+            // Configure create options per os specified
+            var createOptions = JsonConvert.SerializeObject(new {
+                Hostname = kModuleName,
+                Cmd = new[] {
+                "PkiRootPath=" + TestConstants.PublishedNodesFolderLegacy + "/pki",
+                "--aa",
+                "--pf=" + TestConstants.PublishedNodesFullNameLegacy
+            },
+                HostConfig = new {
+                    Binds = new[] {
+                    TestConstants.PublishedNodesFolderLegacy + "/:" + TestConstants.PublishedNodesFolderLegacy
+                    }
+                }
+            }).Replace("\"", "\\\"");
+
+            var server = TestConstants.MicrosoftContainerRegistry;
+            var version = kVersion;
+            var image = $"{server}/iotedge/opc-publisher:{version}";
+
+            // Return deployment modules object
+            var content = @"
+            {
+                ""$edgeAgent"": {
+                    " + registryCredentials + @"
+                    ""properties.desired.modules." + kModuleName + @""": {
+                        ""settings"": {
+                            ""image"": """ + image + @""",
+                            ""createOptions"": """ + createOptions + @"""
+                        },
+                        ""type"": ""docker"",
+                        ""status"": ""running"",
+                        ""restartPolicy"": ""always"",
+                        ""version"": """ + (version == "latest" ? "1.0" : version) + @"""
+                    }
+                },
+                ""$edgeHub"": {
+                    ""properties.desired.routes." + kModuleName + @"ToUpstream"": ""FROM /messages/modules/" + kModuleName + @"/* INTO $upstream"",
+                    ""properties.desired.routes.leafToUpstream"": ""FROM /messages/* WHERE NOT IS_DEFINED($connectionModuleId) INTO $upstream""
+                }
+            }";
+            return JsonConvert.DeserializeObject<IDictionary<string, IDictionary<string, object>>>(content);
+        }
+
+        private const string kVersion = "2.5.5";
+        private const string kModuleName = "publisher_standalone_legacy";
+        private const string kDeploymentName = "__default-opcpublisher-standalone-legacy";
+        private const string kTargetCondition = "(tags.__type__ = 'iiotedge' AND IS_DEFINED(tags.unmanaged))";
+    }
+}
+
