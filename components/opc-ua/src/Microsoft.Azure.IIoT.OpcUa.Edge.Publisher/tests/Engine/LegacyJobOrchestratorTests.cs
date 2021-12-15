48132fdda95d5f4fe6ea681f39b758852332dd47
Cristian Pogacean <crpogace@microsoft.com>
2021-12-15 13:30:02 +0100

    updateing  unit tests

--- components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Edge.Publisher/tests/Engine/LegacyJobOrchestratorTests.cs
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Edge.Publisher/tests/Engine/LegacyJobOrchestratorTests.cs
@@ -6,20 +6,16 @@
 namespace Microsoft.Azure.IIoT.OpcUa.Edge.Publisher.Tests.Engine {
     using System.Collections.Generic;
     using System.Linq;
-    using System.IO;
     using System.Threading.Tasks;
     using Agent.Framework;
     using Agent.Framework.Models;
     using Diagnostics;
-    using FluentAssertions;
     using Models;
     using Module;
     using Moq;
     using Publisher.Engine;
     using Serializers.NewtonSoft;
     using Xunit;
-    using Microsoft.Azure.IIoT.Serializers;
-    using Microsoft.Azure.IIoT.OpcUa.Publisher.Config.Models;
     using static Microsoft.Azure.IIoT.Modules.OpcUa.Publisher.Agent.PublisherJobsConfiguration;
 
     /// <summary>
@@ -84,85 +80,6 @@ namespace Microsoft.Azure.IIoT.OpcUa.Edge.Publisher.Tests.Engine {
             Assert.Null(job3);
 
             Assert.NotEqual(job1.Job.Id, job2.Job.Id);
-        }
-
-        [Theory]
-        [InlineData("Engine/publishednodes.json")]
-        [InlineData("Engine/publishednodeswithoptionalfields.json")]
-        [InlineData("Engine/pn_assets.json")]
-        [InlineData("Engine/pn_assets_with_optional_fields.json")]
-        public async Task DmApiPublishNodesonOnEmptyConfiguration(string publishedNodesFile) {
-            var legacyCliModelProviderMock = new Mock<ILegacyCliModelProvider>();
-            var agentConfigProviderMock = new Mock<IAgentConfigProvider>();
-            var identityMock = new Mock<IIdentity>();
-            var newtonSoftJsonSerializer = new NewtonSoftJsonSerializer();
-            var jobSerializer = new PublisherJobSerializer(newtonSoftJsonSerializer);
-            var publishedNodesJobConverter = new PublishedNodesJobConverter(TraceLogger.Create(), newtonSoftJsonSerializer);
-
-            var legacyCliModel = new LegacyCliModel { PublishedNodesFile = "Engine/empty_pn.json", PublishedNodesSchemaFile = "Storage/publishednodesschema.json" };
-            legacyCliModelProviderMock.Setup(p => p.LegacyCliModel).Returns(legacyCliModel);
-            agentConfigProviderMock.Setup(p => p.Config).Returns(new AgentConfigModel());
-
-            var orchestrator = new LegacyJobOrchestrator(publishedNodesJobConverter, legacyCliModelProviderMock.Object, agentConfigProviderMock.Object, jobSerializer, TraceLogger.Create(), identityMock.Object);
-
-            using var payloads = new StreamReader(publishedNodesFile);
-            var publishNodesRequest = newtonSoftJsonSerializer.Deserialize<List<PublishedNodesEntryModel>>(await payloads.ReadToEndAsync());
-
-            foreach (var request in publishNodesRequest) {
-                var publishNodesResult = await orchestrator.PublishNodesAsync(request).ConfigureAwait(false);
-                publishNodesResult.First().Should().Be("Succeeded");
-            }
-
-            var tasks = new List<Task<JobProcessingInstructionModel>>();
-            for (var i = 0; i < 10; i++) {
-                tasks.Add(orchestrator.GetAvailableJobAsync(i.ToString(), new JobRequestModel()));
-            }
-
-            await Task.WhenAll(tasks);
-
-            tasks.Count(t => t.Result != null).Should().Be(publishNodesRequest.Count);
-            var distinctConfigurations = tasks
-                .Where(t => t.Result != null)
-                .Select(t => t.Result.Job.JobConfiguration)
-                .Distinct();
-            distinctConfigurations.Count().Should().Be(publishNodesRequest.Count);
-        }
-
-
-        [Theory]
-        [InlineData("Engine/publishednodes.json")]
-        [InlineData("Engine/publishednodeswithoptionalfields.json")]
-        [InlineData("Engine/pn_assets.json")]
-        [InlineData("Engine/pn_assets_with_optional_fields.json")]
-        public async Task DmApiUnPublishNodesonToEmptyConfiguration(string publishedNodesFile) {
-            var legacyCliModelProviderMock = new Mock<ILegacyCliModelProvider>();
-            var agentConfigProviderMock = new Mock<IAgentConfigProvider>();
-            var identityMock = new Mock<IIdentity>();
-            var newtonSoftJsonSerializer = new NewtonSoftJsonSerializer();
-            var jobSerializer = new PublisherJobSerializer(newtonSoftJsonSerializer);
-            var publishedNodesJobConverter = new PublishedNodesJobConverter(TraceLogger.Create(), newtonSoftJsonSerializer);
-
-            var legacyCliModel = new LegacyCliModel { PublishedNodesFile = publishedNodesFile, PublishedNodesSchemaFile = "Storage/publishednodesschema.json" };
-            legacyCliModelProviderMock.Setup(p => p.LegacyCliModel).Returns(legacyCliModel);
-            agentConfigProviderMock.Setup(p => p.Config).Returns(new AgentConfigModel());
-
-            var orchestrator = new LegacyJobOrchestrator(publishedNodesJobConverter, legacyCliModelProviderMock.Object, agentConfigProviderMock.Object, jobSerializer, TraceLogger.Create(), identityMock.Object);
-
-            using var payloads = new StreamReader(publishedNodesFile);
-            var unpublishNodesRequest = newtonSoftJsonSerializer.Deserialize<List<PublishedNodesEntryModel>>(await payloads.ReadToEndAsync());
-
-            foreach (var request in unpublishNodesRequest) {
-                var unpublishNodesResult = await orchestrator.UnpublishNodesAsync(request).ConfigureAwait(false);
-                unpublishNodesResult.First().Should().Be("Succeeded");
-            }
-
-            var tasks = new List<Task<JobProcessingInstructionModel>>();
-            for (var i = 0; i < 10; i++) {
-                tasks.Add(orchestrator.GetAvailableJobAsync(i.ToString(), new JobRequestModel()));
-            }
-
-            await Task.WhenAll(tasks);
-            tasks.Count(t => t.Result != null).Should().Be(0);
         }
     }
 }
