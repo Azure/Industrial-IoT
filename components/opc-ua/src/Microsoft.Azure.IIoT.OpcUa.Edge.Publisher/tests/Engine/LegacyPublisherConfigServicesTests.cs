bfd05c349ce75f274c189db1de7b8cb9f2ec0550
Karapet Kostandyan <kakostan@microsoft.com>
2021-12-21 10:10:45 +0100

    Added stress test with 100 endpoints. (#1507)
    * Added stress test with 100 endpoints.
    * Implemented review comments.

--- components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Edge.Publisher/tests/Engine/LegacyPublisherConfigServicesTests.cs
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Edge.Publisher/tests/Engine/LegacyPublisherConfigServicesTests.cs
@@ -22,11 +22,32 @@ namespace Microsoft.Azure.IIoT.OpcUa.Edge.Publisher.Tests.Engine {
     using Serializers.NewtonSoft;
     using Xunit;
     using static Microsoft.Azure.IIoT.Modules.OpcUa.Publisher.Agent.PublisherJobsConfiguration;
+    using System.Text;
+    using System;
+    using Microsoft.Azure.IIoT.OpcUa.Publisher.Models;
 
     /// <summary>
     /// Tests the Direct methods configuration for the LegacyJobOrchestrator class
     /// </summary>
-    public class LegacyPublisherConfigServicesTests {
+    public class LegacyPublisherConfigServicesTests : IDisposable {
+
+        private readonly string _tempPublishedNodesFile;
+
+        public LegacyPublisherConfigServicesTests() {
+            _tempPublishedNodesFile = Path.GetTempFileName();
+        }
+
+        public void Dispose() {
+            try {
+                // Remove temporary published nodes file if one was created.
+                if (File.Exists(_tempPublishedNodesFile)) {
+                    File.Delete(_tempPublishedNodesFile);
+                }
+            }
+            catch (Exception) {
+                // Nothign to do.
+            }
+        }
 
         [Theory]
         [InlineData("Engine/publishednodes.json")]
@@ -266,6 +287,138 @@ namespace Microsoft.Azure.IIoT.OpcUa.Edge.Publisher.Tests.Engine {
             distinctConfigurations.Count()
                 .Should()
                 .Be(2);
+        }
+
+        [Fact]
+        public async Task PublishNodesStressTest() {
+            var legacyCliModelProviderMock = new Mock<ILegacyCliModelProvider>();
+            var agentConfigProviderMock = new Mock<IAgentConfigProvider>();
+            var identityMock = new Mock<IIdentity>();
+            var newtonSoftJsonSerializer = new NewtonSoftJsonSerializer();
+            var jobSerializer = new PublisherJobSerializer(newtonSoftJsonSerializer);
+            var publishedNodesJobConverter = new PublishedNodesJobConverter(TraceLogger.Create(), newtonSoftJsonSerializer);
+
+            using (var fileStream = new FileStream(_tempPublishedNodesFile, FileMode.Open, FileAccess.Write)) {
+                fileStream.Write(Encoding.UTF8.GetBytes("[]"));
+            }
+
+            var legacyCliModel = new LegacyCliModel {
+                PublishedNodesFile = _tempPublishedNodesFile,
+                PublishedNodesSchemaFile = "Storage/publishednodesschema.json"
+            };
+            legacyCliModelProviderMock.Setup(p => p.LegacyCliModel).Returns(legacyCliModel);
+            agentConfigProviderMock.Setup(p => p.Config).Returns(new AgentConfigModel());
+
+            var orchestrator = new LegacyJobOrchestrator(
+                publishedNodesJobConverter,
+                legacyCliModelProviderMock.Object,
+                agentConfigProviderMock.Object,
+                jobSerializer,
+                TraceLogger.Create(),
+                identityMock.Object
+            );
+
+            var numberOfEndpoints = 100;
+            var numberOfNodes = 1000;
+
+            var payload = new List<PublishedNodesEntryModel>();
+            for (int endpointIndex = 0; endpointIndex < numberOfEndpoints; ++endpointIndex) {
+                var model = new PublishedNodesEntryModel {
+                    EndpointUrl = new Uri($"opc.tcp://server{endpointIndex}:49580"),
+                };
+
+                model.OpcNodes = new List<OpcNodeModel>();
+                for (var nodeIndex = 0; nodeIndex < numberOfNodes; ++nodeIndex) {
+                    model.OpcNodes.Add(new OpcNodeModel {
+                        Id = $"ns=2;s=Node-Server-{nodeIndex}",
+                    });
+                }
+
+                payload.Add(model);
+            }
+
+            // Publish all nodes.
+            foreach (var request in payload) {
+                var publishNodesResult = await orchestrator.PublishNodesAsync(request).ConfigureAwait(false);
+                publishNodesResult.First()
+                    .Should()
+                    .Be("Succeeded");
+            }
+
+            async Task CheckEndpointsAndNodes(
+                int expectedNumberOfEndpoints,
+                int expectedNumberOfNodes
+            ) {
+                var tasks = new List<Task<JobProcessingInstructionModel>>();
+                for (var i = 0; i < expectedNumberOfEndpoints + 1; i++) {
+                    tasks.Add(orchestrator.GetAvailableJobAsync(i.ToString(), new JobRequestModel()));
+                }
+
+                await Task.WhenAll(tasks).ConfigureAwait(false);
+                tasks.Count(t => t.Result != null)
+                    .Should()
+                    .Be(expectedNumberOfEndpoints);
+
+                var distinctConfigurations = tasks
+                    .Where(t => t.Result != null)
+                    .Select(t => t.Result.Job.JobConfiguration)
+                    .Distinct();
+                distinctConfigurations.Count()
+                    .Should()
+                    .Be(expectedNumberOfEndpoints);
+
+                var writerGroups = tasks
+                    .Where(t => t.Result != null)
+                    .Select(t => jobSerializer.DeserializeJobConfiguration(
+                        t.Result.Job.JobConfiguration, t.Result.Job.JobConfigurationType) as WriterGroupJobModel);
+                writerGroups.Select(
+                        jobModel => jobModel.WriterGroup.DataSetWriters
+                        .Select(writer => writer.DataSet.DataSetSource.PublishedVariables.PublishedData.Count())
+                        .Sum()
+                     ).Count(v => v == expectedNumberOfNodes)
+                     .Should()
+                     .Be(expectedNumberOfEndpoints);
+
+            }
+
+            // Check
+            await CheckEndpointsAndNodes(numberOfEndpoints, numberOfNodes).ConfigureAwait(false);
+
+            // Publish one more node for each endpoint.
+            var payloadDiff = new List<PublishedNodesEntryModel>();
+            for (int endpointIndex = 0; endpointIndex < numberOfEndpoints; ++endpointIndex) {
+                var model = new PublishedNodesEntryModel {
+                    EndpointUrl = new Uri($"opc.tcp://server{endpointIndex}:49580"),
+                    OpcNodes = new List<OpcNodeModel> {
+                        new OpcNodeModel {
+                            Id = $"ns=2;s=Node-Server-{numberOfNodes}",
+                        }
+                    }
+                };
+
+                payloadDiff.Add(model);
+            }
+
+            foreach (var request in payloadDiff) {
+                var publishNodesResult = await orchestrator.PublishNodesAsync(request).ConfigureAwait(false);
+                publishNodesResult.First()
+                    .Should()
+                    .Be("Succeeded");
+            }
+
+            // Check
+            await CheckEndpointsAndNodes(numberOfEndpoints, numberOfNodes + 1).ConfigureAwait(false);
+
+            // Unpublish new nodes for each endpoint.
+            foreach (var request in payloadDiff) {
+                var publishNodesResult = await orchestrator.UnpublishNodesAsync(request).ConfigureAwait(false);
+                publishNodesResult.First()
+                    .Should()
+                    .Be("Succeeded");
+            }
+
+            // Check
+            await CheckEndpointsAndNodes(numberOfEndpoints, numberOfNodes).ConfigureAwait(false);
         }
     }
 }
