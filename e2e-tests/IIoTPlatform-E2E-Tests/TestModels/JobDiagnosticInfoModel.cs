c4cd06bd10f244978743c44ad0cc2bc4eef77745
Karapet Kostandyan <kakostan@microsoft.com>
2022-02-10 22:38:43 +0100

    Fixing location of JobDiagnosticInfoModel so that it does not interfere with the main solution.

--- e2e-tests/IIoTPlatform-E2E-Tests/TestModels/JobDiagnosticInfoModel.cs
+++ e2e-tests/IIoTPlatform-E2E-Tests/TestModels/JobDiagnosticInfoModel.cs
@@ -3,7 +3,7 @@
 //  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
 // ------------------------------------------------------------
 
-namespace Microsoft.Azure.IIoT.OpcUa.Api.Publisher.Models {
+namespace IIoTPlatform_E2E_Tests.TestModels {
 
     using System;
 
