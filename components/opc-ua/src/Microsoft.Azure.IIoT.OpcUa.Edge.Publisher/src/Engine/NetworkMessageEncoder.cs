cb7038e50d0c7169a74311f827f5603c5e5c5fb8
Cristian Pogacean <crpogace@microsoft.com>
2022-03-15 15:04:46 +0100

    fix duplicate display names issue

--- components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Edge.Publisher/src/Engine/NetworkMessageEncoder.cs
+++ components/opc-ua/src/Microsoft.Azure.IIoT.OpcUa.Edge.Publisher/src/Engine/NetworkMessageEncoder.cs
@@ -363,8 +363,10 @@ namespace Microsoft.Azure.IIoT.OpcUa.Edge.Publisher.Engine {
                         DataSetWriterGroup = message.WriterGroup.WriterGroupId,
                         MessageId = message.SequenceNumber.ToString()
                     };
-                    var notificationQueues = message.Notifications.GroupBy(m => m.NodeId)
-                        .Select(c => new Queue<MonitoredItemNotificationModel>(c.ToArray())).ToArray();
+                    var notificationQueues = message.Notifications
+                        .GroupBy(m => !string.IsNullOrEmpty(m.Id) ?m.Id : m.NodeId)
+                        .Select(c => new Queue<MonitoredItemNotificationModel>(c.ToArray()))
+                        .ToArray();
 
                     while (notificationQueues.Any(q => q.Any())) {
                         var payload = notificationQueues
@@ -372,14 +374,10 @@ namespace Microsoft.Azure.IIoT.OpcUa.Edge.Publisher.Engine {
                             .Where(s => s != null)
                             .ToDictionary(
                                 //  Identifier to show for notification in payload of IoT Hub method
-                                //  Prio 1: DataSetFieldId (need to be read from message)
-                                //  Prio 2: DisplayName - nothing to do, because notification.Id already contains DisplayName
+                                //  Prio 1: Id = DataSetFieldId - if already configured
+                                //  Prio 2: Id = DisplayName - if already configured
                                 //  Prio 3: NodeId as configured
-                                s => !string.IsNullOrEmpty(s.Id) ?
-                                        s.Id :
-                                        !string.IsNullOrEmpty(s.DisplayName) ?
-                                            s.DisplayName :
-                                            s.NodeId,
+                                s => !string.IsNullOrEmpty(s.Id) ? s.Id : s.NodeId,
                                 s => s.Value
                             );
 
