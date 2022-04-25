0ebc1e9f7289646b6a6f185d28361f3733beee2e
Karapet Kostandyan <kakostan@microsoft.com>
2022-04-25 11:31:35 +0200

    Added copyright notice and fixed order of using directives.

--- modules/src/Microsoft.Azure.IIoT.Modules.OpcUa.Publisher/tests/PublisherIntegrationTestBase.cs
+++ modules/src/Microsoft.Azure.IIoT.Modules.OpcUa.Publisher/tests/PublisherIntegrationTestBase.cs
@@ -1,3 +1,8 @@
+// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
 namespace Microsoft.Azure.IIoT.Modules.OpcUa.Publisher.Tests {
     using Autofac;
     using Microsoft.Azure.IIoT.Agent.Framework;
