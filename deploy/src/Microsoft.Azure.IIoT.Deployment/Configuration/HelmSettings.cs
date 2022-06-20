886a1a98ea86be3864a6d584f36471bf09a5ca0a
Karapet Kostandyan <kakostan@microsoft.com>
2022-06-20 10:45:50 +0200

    IAI: Changed deployment to use 0.4.3 version of Helm chart and 2.8.3 version of platform.

--- deploy/src/Microsoft.Azure.IIoT.Deployment/Configuration/HelmSettings.cs
+++ deploy/src/Microsoft.Azure.IIoT.Deployment/Configuration/HelmSettings.cs
@@ -11,9 +11,9 @@ namespace Microsoft.Azure.IIoT.Deployment.Configuration {
     class HelmSettings {
 
         // Defaults
-        public static string _defaultRepoUrl = "https://microsoft.github.io/charts/repo";
-        public static string _defaultChartVersion = "0.4.2";
-        public static string _defaultImageTag = "2.8.2";
+        public static string _defaultRepoUrl = "https://azure.github.io/Industrial-IoT/helm";
+        public static string _defaultChartVersion = "0.4.3";
+        public static string _defaultImageTag = "2.8.3";
         public static string _defaultImageNamespace = "";
         public static string _defaultContainerRegistryServer = "mcr.microsoft.com";
         public static string _defaultContainerRegistryUsername = "";
