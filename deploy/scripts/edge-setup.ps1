520eec0d4f17c37295c199bc79e4b732ca15e5f9
Marc Schier <marcschier@hotmail.com>
2022-10-22 13:45:26 +0200

    1.4 config

--- deploy/scripts/edge-setup.ps1
+++ deploy/scripts/edge-setup.ps1
@@ -23,36 +23,49 @@ $enrollPath = join-path $path dps-enroll.ps1
 
 $file = "/etc/aziot/config.toml"
 if (Test-Path $file) {
-    $backup = "$($file)-backup"
-    if (Test-Path $backup) {
-        Write-Host "Already configured."
-        return
-    }
-    $configtoml = Get-Content $file -Raw
-    if ([string]::IsNullOrWhiteSpace($configtoml)) {
-        throw "$($file) empty."
-    }
-    $configtoml | Out-File $backup -Force
-}
-else {
-    throw "$($file) does not exist."
+    Write-Host "Already configured."
+    return
 }
 
 Write-Host "Create new IoT Edge enrollment."
 $enrollment = & $enrollPath -dpsConnString $dpsConnString -os Linux
 Write-Host "Configure and initialize IoT Edge on Linux using enrollment information."
 
-# comment out existing 
-$configtoml = $configtoml.Replace("`n[provisioning]", "`n# [provisioning]")
-$configtoml = $configtoml.Replace("`nsource", "`n# source")
-$configtoml = $configtoml.Replace("`ndevice_connection_string", "`n# device_connection_string")
-$configtoml = $configtoml.Replace("`ndynamic_reprovisioning", "`n# dynamic_reprovisioning")
-
 # add dps setting
+$configtoml = "`nauto_reprovisioning_mode = `"OnErrorOnly`""
 $configtoml += "`n"
-$configtoml += "`n########################################################################"
-$configtoml += "`n# DPS symmetric key provisioning configuration - added by edge-setup.ps1 #"
-$configtoml += "`n########################################################################"
+$configtoml += "`n[aziot_keys]"
+$configtoml += "`n"
+$configtoml += "`n[preloaded_keys]"
+$configtoml += "`n"
+$configtoml += "`n[cert_issuance]"
+$configtoml += "`n"
+$configtoml += "`n[preloaded_certs]"
+$configtoml += "`n"
+$configtoml += "`n[tpm]"
+$configtoml += "`n"
+$configtoml += "`n[agent]"
+$configtoml += "`nname = `"edgeAgent`""
+$configtoml += "`ntype = `"docker`""
+$configtoml += "`nimagePullPolicy = `"on-create`""
+$configtoml += "`n"
+$configtoml += "`n[agent.config]"
+$configtoml += "`nimage = `"mcr.microsoft.com/azureiotedge-agent:1.4`""
+$configtoml += "`n"
+$configtoml += "`n[agent.config.createOptions]"
+$configtoml += "`n"
+$configtoml += "`n[agent.env]"
+$configtoml += "`n"
+$configtoml += "`n[connect]"
+$configtoml += "`nworkload_uri = `"unix:///var/run/iotedge/workload.sock`""
+$configtoml += "`nmanagement_uri = `"unix:///var/run/iotedge/mgmt.sock`""
+$configtoml += "`n"
+$configtoml += "`n[listen]"
+$configtoml += "`nworkload_uri = `"fd://aziot-edged.workload.socket`""
+$configtoml += "`nmanagement_uri = `"fd://aziot-edged.mgmt.socket`""
+$configtoml += "`nmin_tls_version = `"tls1.0`""
+$configtoml += "`n[watchdog]"
+$configtoml += "`nmax_retries = `"infinite`""
 $configtoml += "`n"
 $configtoml += "`n[provisioning]"
 $configtoml += "`nsource = `"dps`""
@@ -63,8 +76,6 @@ $configtoml += "`n[provisioning.attestation]"
 $configtoml += "`nmethod = `"symmetric_key`""
 $configtoml += "`nregistration_id = `"$($enrollment.registrationId)`""
 $configtoml += "`nsymmetric_key = `"$($enrollment.primaryKey)`""
-$configtoml += "`n"
-$configtoml += "`n########################################################################"
 $configtoml += "`n"
 
 $configtoml | Out-File $file -Force
