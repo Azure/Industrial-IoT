115935271caebb93a0a5f0de32fbbf22f295532f
Marc Schier <marcschier@hotmail.com>
2022-10-28 10:34:54 +0200

    eflow trial

--- deploy/scripts/dsc-install.ps1
+++ deploy/scripts/dsc-install.ps1
@@ -1,6 +1,9 @@
+Install-Module -Name ComputerManagementDsc -RequiredVersion 8.5.0
+
 Configuration InstallWindowsFeatures {
 
     Import-DscResource -ModuleName PsDesiredStateConfiguration
+    Import-DscResource -ModuleName ComputerManagementDsc
 
     Node "localhost" {
 
@@ -13,6 +16,42 @@ Configuration InstallWindowsFeatures {
             Name   = "Hyper-V"
             Ensure = "Present"
             IncludeAllSubFeature = $true
+        }
+
+        WindowsOptionalFeature VirtualMachinePlatform {
+            Name   = "VirtualMachinePlatform"
+            Ensure = "Present"
+        }
+
+        WindowsFeature Hyper-V-Management-Tools {
+            Name = "RSAT-Hyper-V-Tools"
+            Ensure = "Present"
+        }
+
+        WindowsOptionalFeature Microsoft-Hyper-V-Management-PowerShell {
+            Name   = "Microsoft-Hyper-V-Management-PowerShell"
+            Ensure = "Present"
+        }
+
+        WindowsFeature DHCP {
+            Name   = "DHCP"
+            Ensure = "Present"
+            IncludeAllSubFeature = $true
+        }
+
+        WindowsFeature DHCP-Management-Tools {
+            Name = "RSAT-DHCP"
+            Ensure = "Present"
+        }
+
+        WindowsCapability OpenSSH-Client {
+            Name   = "OpenSSH.Client*"
+            Ensure = "Present"
+        }
+
+        WindowsOptionalFeature Microsoft-Windows-Subsystem-Linux {
+            Name   = "Microsoft-Windows-Subsystem-Linux"
+            Ensure = "Present"
         }
     }
 }
--- deploy/scripts/eflow-setup.ps1
+++ deploy/scripts/eflow-setup.ps1
@@ -19,14 +19,16 @@ param(
     [string] $idScope
 )
 
+
 $path = Split-Path $script:MyInvocation.MyCommand.Path
 $enrollPath = join-path $path dps-enroll.ps1
 
 # Set-ExecutionPolicy -ExecutionPolicy AllSigned -Force
 Start-Transcript -path (join-path $path "edge-setup.log")
 
-Write-Host "Create new IoT Edge enrollment."
-$enrollment = & $enrollPath -dpsConnString $dpsConnString -os Windows
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module Subnet -Force
 
 Write-Host "Download IoT Edge installer."
 $msiPath = $([io.Path]::Combine($env:TEMP, 'AzureIoTEdge.msi'))
@@ -37,10 +39,40 @@ Invoke-WebRequest "https://aka.ms/AzEFLOWMSI-CR-X64" -OutFile $msiPath
 Write-Host "Run IoT Edge installer."
 Start-Process -Wait msiexec -ArgumentList "/i","$([io.Path]::Combine($env:TEMP, 'AzureIoTEdge.msi'))","/qn"
 
-Write-Host "Deploy eflow."
-Deploy-Eflow -acceptEula Yes -acceptOptionalTelemetry Yes 
-Write-Host "Provision eflow."
+Get-VmSwitch
+$switch = "NestedSwitch"
+Write-Host "Add virtual switch $($switch)..."
+New-VMSwitch -Name $switch -SwitchType Internal
+$ifIndex = (Get-NetAdapter -Name "vEthernet ($($switch))").ifIndex
+$virtualSwitchIp = Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $ifIndex
+$subnet = Get-Subnet -IP $virtualSwitchIp -MaskBits 24
+New-NetIPAddress -IPAddress $subnet.HostAddresses[0] -PrefixLength $subnet.MaskBits -InterfaceIndex  $ifIndex
+New-NetNat -Name $switch -InternalIPInterfaceAddressPrefix "{$($subnet.NetworkAddress)}/$($subnet.MaskBits)"
+
+Write-Host "Configure DHCP"
+cmd.exe /c "netsh dhcp add securitygroups"
+Restart-Service dhcpserver
+# select a set of 100 addresses
+$startIp = $subnet.HostAddresses[100]
+$endIp = $subnet.HostAddresses[200]
+Add-DhcpServerV4Scope -Name "AzureIoTEdgeScope" -StartRange $startIp -EndRange $endIp -SubnetMask $subnet.SubnetMask -State Active
+Set-DhcpServerV4OptionValue -ScopeID $subnet.NetworkAddress -Router $subnet.HostAddresses[0]
+Restart-service dhcpserver
+
+ipconfig /all
+
+Write-Host "Deploy eflow with switch $($switch)."
+Deploy-Eflow -acceptEula Yes -acceptOptionalTelemetry Yes -vSwitchType "Internal" -vSwitchName $switch
+
+Get-EflowVmAddr
+Get-EflowVmEndpoint
+Get-EflowNetwork -vSwitchName $switch
+
+Write-Host "Create new IoT Edge enrollment in DPS."
+$enrollment = & $enrollPath -dpsConnString $dpsConnString -os Windows
+
+Write-Host "Provision eflow with DPS registration $($enrollment.registrationId) in DPS scope $($idScope)."
 Provision-EflowVm -provisioningType DpsSymmetricKey -scopeId $idScope ´
     -registrationId $enrollment.registrationId -symmKey $enrollment.primaryKey
-   
+
 Write-Host "Eflow provisioned and running."
