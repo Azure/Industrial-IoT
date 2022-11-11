afba92b966159daf228c2a56584be3d548ac65b6
Marc Schier <marcschier@hotmail.com>
2022-11-11 12:44:34 +0100

    Fix build

---
+++ e2e-tests/IIoTPlatform-E2E-Tests/TestExtensions/IIoTStandaloneTestCollection.cs
@@ -0,0 +1,15 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace IIoTPlatform_E2E_Tests.TestExtensions {
+    using Xunit;
+
+    [CollectionDefinition("IIoT Standalone Test Collection", DisableParallelization = true)]
+    public class IIoTStandaloneTestCollection : ICollectionFixture<IIoTMultipleNodesTestContext> {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
