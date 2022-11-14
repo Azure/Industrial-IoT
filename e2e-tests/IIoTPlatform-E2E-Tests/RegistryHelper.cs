0adc624fc233a35040238d1f65521c06bbef803f
Marc Schier <marcschier@hotmail.com>
2022-11-14 08:12:54 +0100

    Revert partial changes

--- e2e-tests/IIoTPlatform-E2E-Tests/RegistryHelper.cs
+++ e2e-tests/IIoTPlatform-E2E-Tests/RegistryHelper.cs
@@ -56,12 +56,7 @@ namespace IIoTPlatform_E2E_Tests {
                         return;
                     }
 
-                    // We've observed situations when even after the above waits the module did not yet restart.
-                    // That leads to situations where the publishing of nodes happens just before the restart to apply
-                    // new container creation options. After restart persisted nodes are picked up, but on the telemetry side
-                    // the restart causes dropped messages to be detected. That happens because just before the restart OPC Publisher
-                    // manages to send some telemetry. This wait makes sure that we do not run the test while restart is happening.
-                    await Task.Delay(TestConstants.AwaitInitInMilliseconds, ct).ConfigureAwait(false);
+                    await Task.Delay(TestConstants.DefaultDelayMilliseconds, ct).ConfigureAwait(false);
                 }
             }
             catch (OperationCanceledException) {
--- e2e-tests/IIoTPlatform-E2E-Tests/TestHelper.cs
+++ e2e-tests/IIoTPlatform-E2E-Tests/TestHelper.cs
@@ -814,13 +814,12 @@ namespace IIoTPlatform_E2E_Tests {
                     };
                 }
                 catch (Exception e) {
-                    PrettyPrintException(e, context.OutputHelper);
-                    if (e.Message.Contains("The operation failed because the requested device isn't online") && ++attempt < 3) {
-                        // Try again twice after waiting
+                    if (e.Message.Contains("The operation failed because the requested device isn't online") && ++attempt < 60) {
                         context.OutputHelper.WriteLine("Device is not online, trying again to call device after delay...");
-                        await Task.Delay(TestConstants.AwaitInitInMilliseconds, ct).ConfigureAwait(false);
+                        await Task.Delay(TestConstants.DefaultDelayMilliseconds, ct).ConfigureAwait(false);
                         continue;
                     }
+					PrettyPrintException(e, context.OutputHelper);
                     throw;
                 }
             }
