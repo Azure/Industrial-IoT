abbd65adef38b0848dfb2f49b08a8712eaccbc41
Marc Schier <marcschier@hotmail.com>
2022-11-23 10:08:08 +0100

    Use workspace id not resource id

--- common/src/Microsoft.Azure.IIoT.Core/src/Deploy/Runtime/LogWorkspaceConfig.cs
+++ common/src/Microsoft.Azure.IIoT.Core/src/Deploy/Runtime/LogWorkspaceConfig.cs
@@ -12,13 +12,11 @@ namespace Microsoft.Azure.IIoT.Deploy.Runtime {
     /// </summary>
     public class LogWorkspaceConfig : ConfigBase, ILogWorkspaceConfig {
 
-        private const string kWorkspaceId = "Docker:WorkspaceId";
-        private const string kWorkspaceKey = "Docker:WorkspaceKey";
+        private const string kWorkspaceId = "AzureMonitor:WorkspaceId";
+        private const string kWorkspaceKey = "AzureMonitor:WorkspaceKey";
         private const string kSubscriptionId = "SubscriptionId";
         private const string kResourceGroupName = "ResourceGroupName";
         private const string kIoTHubConnectionString = "IoTHubConnectionString";
-
-
 
         /// <inheritdoc/>
         public string LogWorkspaceId => GetStringOrDefault(kWorkspaceId,
@@ -29,14 +27,14 @@ namespace Microsoft.Azure.IIoT.Deploy.Runtime {
             () => GetStringOrDefault(PcsVariable.PCS_WORKSPACE_KEY));
 
         /// <inheritdoc/>
-        public string IoTHubResourceId 
-        {
+        public string IoTHubResourceId {
             get {
                 string subscriptionId = GetStringOrDefault(kSubscriptionId, () => GetStringOrDefault(PcsVariable.PCS_SUBSCRIPTION_ID));
                 string resourceGroup = GetStringOrDefault(kResourceGroupName, () => GetStringOrDefault(PcsVariable.PCS_RESOURCE_GROUP));
                 if (string.IsNullOrEmpty(resourceGroup) || string.IsNullOrEmpty(subscriptionId)) {
                     return string.Empty;
-                } else {
+                }
+                else {
                     return "/subscriptions/" + subscriptionId + "/resourceGroups/" + resourceGroup +
                             "/providers/Microsoft.Devices/IotHubs/" +
                             ConnectionString.Parse(GetStringOrDefault(kIoTHubConnectionString,
@@ -44,7 +42,7 @@ namespace Microsoft.Azure.IIoT.Deploy.Runtime {
                 }
             }
         }
-            
+
         /// <summary>
         /// Configuration constructor
         /// </summary>
