eb88e982274be432d2f20e9f71989cc709fb7b85
Marc Schier <marcschier@hotmail.com>
2023-02-28 16:55:17 +0100

    Container publishing

--- src/Azure.IIoT.OpcUa.Services.WebApi/src/Controllers/PublishController.cs
+++ src/Azure.IIoT.OpcUa.Services.WebApi/src/Controllers/PublishController.cs
@@ -17,6 +17,7 @@ namespace Azure.IIoT.OpcUa.Services.WebApi.Controllers
     using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Threading.Tasks;
+    using Furly.Extensions.Http;
 
     /// <summary>
     /// Value and Event publishing services
