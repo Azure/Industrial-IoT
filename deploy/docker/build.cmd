282c61cfc3c62f09c942c12508acce38460175f5
Marc Schier <marcschier@hotmail.com>
2023-09-13 11:01:48 +0200

    Revert back opc stack

--- deploy/docker/build.cmd
+++ deploy/docker/build.cmd
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 
 set BASEIMAGE=/p:ContainerBaseImage=mcr.microsoft.com/dotnet/aspnet:7.0-alpine-amd64
 set CMDLINE=-r linux-musl-x64 --self-contained false /t:PublishContainer
@@ -6,10 +7,3 @@ set PROJECT=../../src/Azure.IIoT.OpcUa.Publisher.Module/src/Azure.IIoT.OpcUa.Pub
 
 dotnet publish %PROJECT% -c Release %CMDLINE% %BASEIMAGE% /p:ContainerImageTag=latest
 dotnet publish %PROJECT% -c Debug %CMDLINE% %BASEIMAGE% /p:ContainerImageTag=debug
-
-set BASEIMAGE=
-set CMDLINE=
-set PROJECT=
-
-set DOCKER_REGISTRY=""
-set OPC_PUBLISHER_TAG=latest
