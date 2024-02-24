1efc1a9218af0a2877db81cc5f72e84520e9944f
Marc Schier <mschier@microsoft.com>
2024-02-24 16:38:45 +0100

    Remove file

--- src/Azure.IIoT.OpcUa.Publisher/src/Services/IPublishQueue.cs
+++
@@ -1,9 +0,0 @@
-﻿// ------------------------------------------------------------
-//  Copyright (c) Microsoft Corporation.  All rights reserved.
-//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
-// ------------------------------------------------------------
-
-namespace Azure.IIoT.OpcUa.Publisher.Services
-{
-    using Azure.IIoT.OpcUa.Publisher.Stack;
-}
