e03d06f81674ddf6a79021c445935d8dcfcf8a77
Marc Schier <mschier@microsoft.com>
2024-06-09 15:03:21 +0200

    udpate

--- tools/e2etesting/SetVariables.ps1
+++ tools/e2etesting/SetVariables.ps1
@@ -2,30 +2,25 @@
     [string] $BranchName,
     [string] $Region,
     [string] $ImageTag,
-    [string] $ContainerRegistryServer
+    [string] $ContainerRegistry
 )
 
 # Stop execution when an error occurs.
 $ErrorActionPreference = "Stop"
 # Set-PSDebug -Trace 2
 
-if (![string]::IsNullOrEmpty($script:BranchName))
+if ([string]::IsNullOrWhiteSpace($script:ContainerRegistry))
 {
-    if ($script:BranchName.StartsWith("refs/heads/"))
-    {
-        $script:BranchName = $script:BranchName.Replace("refs/heads/", "")
-    }
-}
-else
-{
-    $script:BranchName = "main"
+    throw "Must provide container registry name"
 }
 
-if ([string]::IsNullOrEmpty($script:ImageTag))
+$registry = $script:ContainerRegistry
+
+if ([string]::IsNullOrWhiteSpace($script:ImageTag))
 {
     $script:ImageTag = "$($env:Version_Prefix)$($env:Version_Prerelease)"
 }
-if ([string]::IsNullOrEmpty($script:ImageTag))
+if ([string]::IsNullOrWhiteSpace($script:ImageTag))
 {
     try
     {
@@ -50,69 +45,41 @@ if ([string]::IsNullOrEmpty($script:ImageTag))
     }
 }
 
-
 # Set namespace name based on branch name
-$imageNamespace = $script:BranchName
-if ($imageNamespace.StartsWith("feature/"))
+if ($registry -eq "industrialiotdev")
 {
-    # dev feature builds
-    $imageNamespace = $imageNamespace.Replace("feature/", "")
-}
-elseif ($imageNamespace.StartsWith("release/") -or ($imageNamespace -eq "releases"))
-{
-    $imageNamespace = "public"
-}
-$imageNamespace = $imageNamespace.Replace("_", "/").Substring(0, [Math]::Min($imageNamespace.Length, 24))
-
-function Get-ContainerRegistrySecret
-{
-    param(
-        [Parameter(Mandatory=$true)]
-        [string] $keyVaultName,
-
-        [Parameter(Mandatory=$true)]
-        [string] $secret
-    )
-    $secretValueSec = Get-AzKeyVaultSecret -VaultName $keyVaultName -Name $secret
-    $ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secretValueSec.SecretValue)
-    try
+    if (![string]::IsNullOrWhiteSpace($script:BranchName))
     {
-        $secretValueText = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
-    }
-    finally
-    {
-        [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
-    }
-    Write-Host "##vso[task.setvariable variable=$($secret)]$($secretValueText)"
-}
-
-if ([string]::IsNullOrEmpty($script:ContainerRegistryServer))
-{
-    if ($imageNamespace -eq "public")
-    {
-        # Release and Preview builds are in staging
-        $registry = "industrialiot"
-        $KeyVaultName = "kv-release-pipeline"
+        if ($script:BranchName.StartsWith("refs/heads/"))
+        {
+            $script:BranchName = $script:BranchName.Replace("refs/heads/", "")
+        }
     }
     else
     {
-        # Feature builds by default into dev registry
-        $registry = "industrialiotdev"
-        $KeyVaultName = "kv-developer-pipeline"
+        $script:BranchName = "main"
     }
 
-    Write-Host "Looking up credentials for $($registry) registry in KV $KeyVaultName."
-    Get-ContainerRegistrySecret -keyVaultName $KeyVaultName -secret "ContainerRegistryPassword"
-    Get-ContainerRegistrySecret -keyVaultName $KeyVaultName -secret "ContainerRegistryServer"
-    Get-ContainerRegistrySecret -keyVaultName $KeyVaultName -secret "ContainerRegistryUsername"
+    $imageNamespace = $script:BranchName
+    if ($imageNamespace.StartsWith("feature/"))
+    {
+        # dev feature builds
+        $imageNamespace = $imageNamespace.Replace("feature/", "")
+    }
+    $imageNamespace = $imageNamespace.Replace("_", "/")
+    $imageNamespace = $imageNamespace.Substring(0, [Math]::Min($imageNamespace.Length, 24))
 }
-else
+elseif ($registry -eq "mcr.microsoft.com")
 {
-    $registry = $script:ContainerRegistryServer
+    $imageNamespace = ""
+}
+else 
+{
+    $imageNamespace = "public"
 }
 
 Write-Host "=============================================================================="
-Write-Host "Use $($script:ImageTag) images in namespace $($imageNamespace) from $($registry)."
+Write-Host "Selected $($script:ImageTag) images in namespace $($imageNamespace) from $($registry)."
 Write-Host "=============================================================================="
 Write-Host ""
 
