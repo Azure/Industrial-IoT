e8831b579557f9ac70a5573c9c59520813867cbf
Marc Schier <marcschier@hotmail.com>
2024-06-12 15:50:10 +0200

    `Add new namespace and update CreateMessage method`
    Added a new namespace `Azure.IIoT.OpcUa.Publisher.Stack` to `JsonNetworkMessageEncoderTests1.cs` and `JsonNetworkMessageEncoderTests2.cs` files. Updated the `CreateMessage` method in both files to use the `PubSubMessageContentFlagHelper` methods for setting `NetworkMessageContentMask`, `DataSetMessageContentMask`, and `Payload` properties instead of direct casting. Reformatted `MetaDataVersion` property for better readability. Added a new `PubSubMessageContentFlagHelper.cs` file that contains methods for converting mask values to their corresponding flags.

--- src/Azure.IIoT.OpcUa/tests/Encoders/PubSub/JsonNetworkMessageEncoderTests2.cs
+++ src/Azure.IIoT.OpcUa/tests/Encoders/PubSub/JsonNetworkMessageEncoderTests2.cs
@@ -7,6 +7,7 @@ namespace Azure.IIoT.OpcUa.Encoders.PubSub
 {
     using Azure.IIoT.OpcUa.Encoders.Models;
     using Azure.IIoT.OpcUa.Publisher.Models;
+    using Azure.IIoT.OpcUa.Publisher.Stack;
     using Furly.Extensions.Serializers;
     using Furly.Extensions.Serializers.Newtonsoft;
     using Opc.Ua;
@@ -716,12 +717,12 @@ namespace Azure.IIoT.OpcUa.Encoders.PubSub
             return new JsonNetworkMessage
             {
                 DataSetClassId = Guid.Parse("5ae1a63a-9757-4aa7-ab71-0d88931266fc"),
-                NetworkMessageContentMask = (NetworkMessageContentFlags)messageMask,
+                NetworkMessageContentMask = PubSubMessageContentFlagHelper.StackToNetworkMessageContentFlags(messageMask),
                 MessageId = () => "9279C0B3-DA88-45A4-AF74-451CEBF82DB0",
                 PublisherId = "MyPublisher",
                 Messages = new List<BaseDataSetMessage> {
                     new JsonDataSetMessage {
-                        DataSetMessageContentMask = (DataSetMessageContentFlags)datasetMask,
+                        DataSetMessageContentMask = PubSubMessageContentFlagHelper.StackToDataSetMessageContentFlags(datasetMask),
                         DataSetWriterId = 100,
                         SequenceNumber = 29766,
                         MetaDataVersion = new ConfigurationVersionDataType
@@ -745,7 +746,7 @@ namespace Azure.IIoT.OpcUa.Encoders.PubSub
                         }
                     },
                     new JsonDataSetMessage {
-                        DataSetMessageContentMask = (DataSetMessageContentFlags)datasetMask,
+                        DataSetMessageContentMask = PubSubMessageContentFlagHelper.StackToDataSetMessageContentFlags(datasetMask),
                         DataSetWriterId = 100,
                         SequenceNumber = 29767,
                         MetaDataVersion = new ConfigurationVersionDataType
@@ -757,7 +758,7 @@ namespace Azure.IIoT.OpcUa.Encoders.PubSub
                         Status = 1073741824,
                         MessageType = MessageType.DeltaFrame,
                         DataSetWriterName = "Writer100",
-                        Payload = new DataSet((DataSetFieldContentFlags)fieldMask) {
+                        Payload = new DataSet(PubSubMessageContentFlagHelper.StackToDataSetFieldContentFlags(fieldMask)) {
     ["Temperature"] = new DataValue(26, StatusCodes.Good,
         DateTime.Parse("2021-09-27T18:45:19.556Z", CultureInfo.InvariantCulture),
         DateTime.Parse("2021-09-27T18:45:19.556Z", CultureInfo.InvariantCulture)),
---
+++ src/Azure.IIoT.OpcUa/tests/Encoders/PubSub/PubSubMessageContentFlagHelper.cs
@@ -0,0 +1,167 @@
+// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace Azure.IIoT.OpcUa.Publisher.Stack
+{
+    using Azure.IIoT.OpcUa.Publisher.Models;
+    using Opc.Ua;
+
+    /// <summary>
+    /// Stack types conversions
+    /// </summary>
+    internal static class PubSubMessageContentFlagHelper
+    {
+        /// <summary>
+        /// Get network message content mask
+        /// </summary>
+        /// <param name="mask"></param>
+        /// <returns></returns>
+        public static NetworkMessageContentFlags StackToNetworkMessageContentFlags(uint mask)
+        {
+            NetworkMessageContentFlags result = 0u;
+            if ((mask & (uint)JsonNetworkMessageContentMask.PublisherId) != 0)
+            {
+                result |= NetworkMessageContentFlags.PublisherId;
+            }
+            if ((mask & (uint)JsonNetworkMessageContentMask.DataSetClassId) != 0)
+            {
+                result |= NetworkMessageContentFlags.DataSetClassId;
+            }
+            if ((mask & (uint)JsonNetworkMessageContentMask.ReplyTo) != 0)
+            {
+                result |= NetworkMessageContentFlags.ReplyTo;
+            }
+            if ((mask & (uint)JsonNetworkMessageContentMask.NetworkMessageHeader) != 0)
+            {
+                result |= NetworkMessageContentFlags.NetworkMessageHeader;
+            }
+            else
+            {
+                // If not set, bits 3, 4 and 5 can also not be set
+                result = 0;
+            }
+            if ((mask & 0x8000000) != 0)
+            {
+                // If monitored item message, then no network message header
+                result = 0;
+            }
+            if ((mask & (uint)JsonNetworkMessageContentMask.DataSetMessageHeader) != 0)
+            {
+                result |= NetworkMessageContentFlags.DataSetMessageHeader;
+            }
+            if ((mask & (uint)JsonNetworkMessageContentMask.SingleDataSetMessage) != 0)
+            {
+                result |= NetworkMessageContentFlags.SingleDataSetMessage;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Get dataset message content mask
+        /// </summary>
+        /// <param name="mask"></param>
+        /// <returns></returns>
+        public static DataSetMessageContentFlags StackToDataSetMessageContentFlags(uint mask)
+        {
+            DataSetMessageContentFlags result = 0u;
+            if ((mask & (uint)JsonDataSetMessageContentMask.Timestamp) != 0)
+            {
+                result |= DataSetMessageContentFlags.Timestamp;
+            }
+            if ((mask & (uint)JsonDataSetMessageContentMask.Status) != 0)
+            {
+                result |= DataSetMessageContentFlags.Status;
+            }
+            if ((mask & (uint)JsonDataSetMessageContentMask.MetaDataVersion) != 0)
+            {
+                result |= DataSetMessageContentFlags.MetaDataVersion;
+            }
+            if ((mask & (uint)JsonDataSetMessageContentMask.SequenceNumber) != 0)
+            {
+                result |= DataSetMessageContentFlags.SequenceNumber;
+            }
+            if ((mask & (uint)JsonDataSetMessageContentMask.DataSetWriterId) != 0)
+            {
+                result |= DataSetMessageContentFlags.DataSetWriterId;
+            }
+            if ((mask & (uint)JsonDataSetMessageContentMask.MessageType) != 0)
+            {
+                result |= DataSetMessageContentFlags.MessageType;
+            }
+            if ((mask & (uint)JsonDataSetMessageContentMask.DataSetWriterName) != 0)
+            {
+                result |= DataSetMessageContentFlags.DataSetWriterName;
+            }
+            if ((mask & (uint)JsonDataSetMessageContentMask.ReversibleFieldEncoding) != 0)
+            {
+                result |= DataSetMessageContentFlags.ReversibleFieldEncoding;
+            }
+
+            //     if (fieldMask != null)
+            //     {
+            //         if ((fieldMask & DataSetFieldContentFlags.NodeId) != 0)
+            //         {
+            //             result |= JsonDataSetMessageContentMaskEx.NodeId;
+            //         }
+            //         if ((fieldMask & DataSetFieldContentFlags.DisplayName) != 0)
+            //         {
+            //             result |= JsonDataSetMessageContentMaskEx.DisplayName;
+            //         }
+            //         if ((fieldMask & DataSetFieldContentFlags.ExtensionFields) != 0)
+            //         {
+            //             result |= JsonDataSetMessageContentMaskEx.ExtensionFields;
+            //         }
+            //         if ((fieldMask & DataSetFieldContentFlags.EndpointUrl) != 0)
+            //         {
+            //             result |= JsonDataSetMessageContentMaskEx.EndpointUrl;
+            //         }
+            //         if ((fieldMask & DataSetFieldContentFlags.ApplicationUri) != 0)
+            //         {
+            //             result |= JsonDataSetMessageContentMaskEx.ApplicationUri;
+            //         }
+            //     }
+            return result;
+        }
+
+        /// <summary>
+        /// Get dataset message content mask
+        /// </summary>
+        /// <param name="mask"></param>
+        /// <returns></returns>
+        public static DataSetFieldContentFlags StackToDataSetFieldContentFlags(uint mask)
+        {
+            DataSetFieldContentFlags result = 0;
+            if ((mask & (uint)DataSetFieldContentMask.StatusCode) != 0)
+            {
+                result |= DataSetFieldContentFlags.StatusCode;
+            }
+            if ((mask & (uint)DataSetFieldContentMask.SourceTimestamp) != 0)
+            {
+                result |= DataSetFieldContentFlags.SourceTimestamp;
+            }
+            if ((mask & (uint)DataSetFieldContentMask.ServerTimestamp) != 0)
+            {
+                result |= DataSetFieldContentFlags.ServerTimestamp;
+            }
+            if ((mask & (uint)DataSetFieldContentMask.SourcePicoSeconds) != 0)
+            {
+                result |= DataSetFieldContentFlags.SourcePicoSeconds;
+            }
+            if ((mask & (uint)DataSetFieldContentMask.ServerPicoSeconds) != 0)
+            {
+                result |= DataSetFieldContentFlags.ServerPicoSeconds;
+            }
+            if ((mask & (uint)DataSetFieldContentMask.RawData) != 0)
+            {
+                result |= DataSetFieldContentFlags.RawData;
+            }
+            if ((mask & 64) != 0)
+            {
+                result |= DataSetFieldContentFlags.SingleFieldDegradeToValue;
+            }
+            return result;
+        }
+    }
+}
