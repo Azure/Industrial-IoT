faba186ef2dec3ba2ce68b846700e69dd6e20bf0
Marc Schier <marcschier@hotmail.com>
2025-02-24 15:03:02 +0100

    Update build script

--- deploy/docker/build.cmd
+++ deploy/docker/build.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set CMDLINE=--self-contained false /t:PublishContainer
+set CMDLINE=--self-contained false /t:PublishContainer -r linux-x64
 set PROJECT=../../src/Azure.IIoT.OpcUa.Publisher.Module/src/Azure.IIoT.OpcUa.Publisher.Module.csproj
 
 dotnet publish %PROJECT% -c Release %CMDLINE% /p:ContainerImageTag=latest
